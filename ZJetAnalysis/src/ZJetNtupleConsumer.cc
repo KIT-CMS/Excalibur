02dea029e058ed73f615d0abd2b8a2874b6b400b
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2014-04-22 15:03:14 +0200

    Additional NTuple quantities

--- ZJetAnalysis/src/ZJetNtupleConsumer.cc
+++ ZJetAnalysis/src/ZJetNtupleConsumer.cc
@@ -16,19 +16,28 @@ void ZJetNtupleConsumer::Init(Pipeline<ZJetTypes>* pset)
 
 	m_valueExtractorMap["metphi"] = [](ZJetEvent const & event, ZJetProduct const & product)
 	{return event.m_met->p4.Phi(); };
+	m_valueExtractorMap["metpt"] = [](ZJetEvent const & event, ZJetProduct const & product)
+	{return event.m_met->p4.Pt(); };
 
 	m_valueExtractorMap["validz"] = [](ZJetEvent const & event, ZJetProduct const & product)
 	{return product.has_valid_z; };
 
 	m_valueExtractorMap["zmass"] = [](ZJetEvent const & event, ZJetProduct const & product)
 	{return product.Z.p4.mass(); };
+	m_valueExtractorMap["zpt"] = [](ZJetEvent const & event, ZJetProduct const & product)
+	{return product.Z.p4.Pt(); };
+	m_valueExtractorMap["zy"] = [](ZJetEvent const & event, ZJetProduct const & product)
+	{return product.Z.p4.Rapidity(); };
 
 	m_valueExtractorMap["nvalidmuons"] = [](ZJetEvent const & event, ZJetProduct const & product)
 	{return product.m_validmuons.size(); };
 	
 	m_valueExtractorMap["jet1pt"] = [&](ZJetEvent const & event, ZJetProduct const & product)
 	{return product.GetLeadingJet(algoname).p4.Pt(); };
-
+	m_valueExtractorMap["jet1eta"] = [&](ZJetEvent const & event, ZJetProduct const & product)
+	{return product.GetLeadingJet(algoname).p4.Eta(); };
+	m_valueExtractorMap["jet1phi"] = [&](ZJetEvent const & event, ZJetProduct const & product)
+	{return product.GetLeadingJet(algoname).p4.Phi(); };
 
 	LambdaNtupleConsumerBase<ZJetTypes>::Init(pset);
 }
