be094295c0d1859a8b00ed2f72b0a56d666718ba
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2014-05-19 19:13:11 +0200

    work path for naf

--- ZJetAnalysis/scripts/ini_ZJet.sh
+++ ZJetAnalysis/scripts/ini_ZJet.sh
@@ -8,3 +8,7 @@ export KAPPAPATH=$CMSSW_BASE/src/Kappa
 export KAPPATOOLSPATH=$CMSSW_BASE/KappaTools
 export ARTUSPATH=$CMSSW_BASE/src/Artus
 export ZJETPATH=$CMSSW_BASE/src/ZJet/ZJetAnalysis
+
+if [[ `hostname` == *naf* ]]; then
+	export ARTUS_WORK_BASE="/afs/desy.de/user/$(echo $USER | head -c 1)/${USER}/artus/"
+	fi
