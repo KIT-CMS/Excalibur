ce0fe676cc84d9c263ec6c2ec4c03d921bcfaeb6
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2014-05-19 15:58:43 +0200

    Correct Zjet CB

--- ZJetAnalysis/python/zjetConfigBase.py
+++ ZJetAnalysis/python/zjetConfigBase.py
@@ -1,38 +1,15 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import logging
-import argparse
-import ZJet.ZJetAnalysis.zjetConfigBase as zjetConfigBase
-import Artus.Utility.logger as logger
-log = logging.getLogger(__name__)
+def getZjetConfig(conf):
 
+	basic_mc_quantities = ["npv", "run", "puweight",
+                "metphi", "metpt",
+                "nvalidelectrons", "validz",
+                "zmass", "zpt", "zy"
+    ]
 
-import sys
-from Artus.Configuration.artusWrapper import ArtusWrapper
-
-
-if __name__ == "__main__":
-
-	#Additional ZJet parser arguments
-	zjetParser = argparse.ArgumentParser(add_help=False)
-	zjetGroup = zjetParser.add_argument_group("ZJet options")
-	zjetGroup.add_argument('-y', '--year', type=int,
-        default=2012,
-        help="data taking period. Default ist %(default)s")
-
-	artusWrapper = ArtusWrapper("ZJet", [zjetParser])
-
-	conf = artusWrapper.getConfig()
-
-	#Get additional Zjet settings from config base
-	zjetconf = zjetConfigBase.getZjetConfig(conf)
-	conf.update(zjetconf)
-	print conf
-	sys.exit()
-
-	artusWrapper.setConfig(conf)
-
-	# Run the wrapper
-	sys.exit(artusWrapper.run())
-
+	if not conf['InputIsData']:
+		for pipeline in conf['Pipelines']:
+			conf['Pipelines'][pipeline]['Quantities'] = basic_mc_quantities
+			
+	return conf
