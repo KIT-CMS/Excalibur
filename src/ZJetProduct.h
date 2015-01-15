6911064139efa956adb36ef7844278e33aeb2af3
Joram Berger <joram.berger@cern.ch>
2015-01-15 19:06:23 +0100

    Adapt data types to Kappa 2.0

--- src/ZJetProduct.h
+++ src/ZJetProduct.h
@@ -16,41 +16,41 @@ public:
 	#TODO update this code so it'll compile with Kappa2.0
 	
 	float m_product;
-	KDataMuons m_validmuons;
-	KDataMuons m_invalidmuons;
-	const KDataMuon * m_decaymuons [2];
+	KMuons m_validmuons;
+	KMuons m_invalidmuons;
+	const KMuon * m_decaymuons [2];
 
 	bool has_valid_z;
 	bool has_valid_genz;
 
-	KDataLV Z;
+	KLV Z;
 	KGenParticle GenZ;
 	KGenParticles m_genmuons;
 
-	KDataPFTaggedJet * GetLeadingJet(std::string const& algoname) const
+	KJet * GetLeadingJet(std::string const& algoname) const
 	{
 		//if (std::string::npos == algoname.find("L1"))
-			return static_cast<KDataPFTaggedJet*> (m_validJets.at(0));
+			return static_cast<KJet*> (m_validJets.at(0));
 		//else
 		//	return SafeMap::Get(m_validjets, algoname).at(0);
 	}
 
-	KDataPFTaggedJet * GetSecondJet(std::string const& algoname) const
+	KJet * GetSecondJet(std::string const& algoname) const
 	{
 		//if (std::string::npos == algoname.find("L1"))
 		//{
-			return static_cast<KDataPFTaggedJet*> (m_validJets.at(1));
-			}
+			return static_cast<KJet*> (m_validJets.at(1));
+			/*}
 		else if (m_validjets.at(algoname).size() < 2)
 		{
-			KDataPFTaggedJet jet;
+			KJet jet;
 			return jet;
 		}
 		return SafeMap::Get(m_validjets, algoname).at(1);
 			
 	}
 
-	float GetMPF(const KDataPFMET* met) const //type1 !!!
+	float GetMPF(const KMET* met) const //type1 !!!
 	{
 		return 1.0f + (Z.p4.Px() * met->p4.Px()
 			+ Z.p4.Py() * met->p4.Py()) /
