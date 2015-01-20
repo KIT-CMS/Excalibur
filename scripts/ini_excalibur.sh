d8f15262a51a5d01273b5e2ff188b13bb67e5fa2
Joram Berger <joram.berger@cern.ch>
2015-01-20 18:04:47 +0100

    Add boost to ini script

--- scripts/ini_excalibur.sh
+++ scripts/ini_excalibur.sh
@@ -5,6 +5,8 @@ source ../Artus/Configuration/scripts/ini_ArtusAnalysis.sh
 
 # set the environment
 export EXCALIBURPATH=$(readlink -e .)
+export BOOSTPATH=$(ls ${VO_CMS_SW_DIR}/${SCRAM_ARCH}/external/boost/* -d | tail -n 1)
+export BOOSTLIB=${BOOSTPATH}/lib/libboost_regex.so.${BOOSTPATH/*\//}
 export KAPPAPATH=$EXCALIBURPATH/../Kappa
 export KAPPATOOLSPATH=$EXCALIBURPATH/../KappaTools
 export ARTUSPATH=$EXCALIBURPATH/../Artus
