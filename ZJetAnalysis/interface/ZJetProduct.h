60816f33f2f117aef9d4e0d2d67c9f7330d83167
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-01-29 18:00:11 +0100

    Comment some stuff that doestn compile with latest Kappa version, decomment in the future

--- ZJetAnalysis/interface/ZJetProduct.h
+++ ZJetAnalysis/interface/ZJetProduct.h
@@ -12,7 +12,9 @@ class ZJetProduct : public KappaProduct
 {
 public:
 	ZJetProduct() : KappaProduct() {};
-
+/*
+	#TODO update this code so it'll compile with Kappa2.0
+	
 	float m_product;
 	KDataMuons m_validmuons;
 	KDataMuons m_invalidmuons;
@@ -38,14 +40,14 @@ public:
 		//if (std::string::npos == algoname.find("L1"))
 		//{
 			return static_cast<KDataPFTaggedJet*> (m_validJets.at(1));
-			/*}
+			}
 		else if (m_validjets.at(algoname).size() < 2)
 		{
 			KDataPFTaggedJet jet;
 			return jet;
 		}
 		return SafeMap::Get(m_validjets, algoname).at(1);
-			*/
+			
 	}
 
 	float GetMPF(const KDataPFMET* met) const //type1 !!!
@@ -55,5 +57,5 @@ public:
 			 (Z.p4.Px() * Z.p4.Px()
 			+ Z.p4.Py() * Z.p4.Py());
 	}
-
+*/
 };
