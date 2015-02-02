80302b814dd205c7e74880c161f43f4638cc097d
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-02-02 11:24:29 +0100

    inputrootZJet.py: Add a function to automatically detect input type (data/MC) and set certain weights accordingly.

--- Plotting/python/input_modules/inputrootZJet.py
+++ Plotting/python/input_modules/inputrootZJet.py
@@ -3,6 +3,13 @@
 """
 """
 
+import logging
+import Artus.Utility.logger as logger
+log = logging.getLogger(__name__)
+
+import ROOT
+import os
+
 import Artus.HarryPlotter.input_modules.inputroot as inputroot
 
 class InputRootZJet(inputroot.InputRoot):
@@ -31,3 +38,27 @@ class InputRootZJet(inputroot.InputRoot):
 			folders = []
 			for algo, corr, folder in zip([plotData.plotdict[i] for i in zjetlist]):
 				folders.append("%s_%s%s" % (folder, algo, corr))
+
+
+	def auto_detect_type_and_modify_weight(self, weight, root_files, plotData,
+								mc_weight="lumi * weight", data_weight=None):
+		""" This function checks the type (data/MC) of the input file(s) and
+			modifies the weights accordingly. Actual implementation is
+			analysis-specific.
+		"""
+		types = []
+		for root_file in root_files:
+			f = ROOT.TFile(root_file)
+			types.append(f.Get("Type"))
+
+		if all([typ == 'mc' for typ in types]) and mc_weight is not None:
+			if plotData.plotdict['lumi'] == None:
+				log.critical("'lumi' is not set, but needed for weights!")
+			mc_weight = mc_weight.replace('lumi', str(plotData.plotdict['lumi']))
+			log.debug("Automatically add MC weights: %s" % mc_weight)
+			return "((%s) * (%s))" % (weight, mc_weight)
+		elif all([typ == 'data' for typ in types]) and data_weight is not None:
+			log.debug("Automatically add Data weights: %s" % data_weight)
+			return "((%s) * (%s))" % (weight, data_weight)
+		else:
+			return weight
