f168af714f1494487c6dc35f6f46274717aa1104
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-02-03 16:38:20 +0100

    Zjet folde/algo/corr

--- Plotting/python/input_modules/inputrootZJet.py
+++ Plotting/python/input_modules/inputrootZJet.py
@@ -19,26 +19,27 @@ class InputRootZJet(inputroot.InputRoot):
 
 		# special arguemnts for custom zjet folder naming conventions
 		self.zjet_input_options = parser.add_argument_group("ZJet input options")
-		self.zjet_input_options.add_argument("--zjetfolders", type=str, nargs='*', default=None,
+		self.zjet_input_options.add_argument("--zjetfolders", type=str, nargs='*', default=['incut'],
 		                                help="zjet folders (all, incut....")
-		self.zjet_input_options.add_argument("--algorithms", type=str, nargs='*', default=None,
+		self.zjet_input_options.add_argument("--algorithms", type=str, nargs='*', default=["AK5PFJetsCHS"],
 		                                help="jet algorithms.")
-		self.zjet_input_options.add_argument("--corrections", type=str, nargs='*', default=None,
+		self.zjet_input_options.add_argument("--corrections", type=str, nargs='*', default=["L1L2L3"],
 		                                help="correction levels.")
 		self.zjet_input_options.set_defaults(files=[os.environ.get(x) for x in ['DATA', 'MC']])
 
 
 	def prepare_args(self, parser, plotData):
-		super(InputRootZJet, self).prepare_args(parser, plotData)
-		
 		# this is needed so one can put together the folder name like in the old
 		# merlin plotting
 		zjetlist =  ["algorithms", "corrections", "zjetfolders"]
 		self.prepare_list_args(plotData, zjetlist)
 		if all( [plotData.plotdict[i] != [None] for i in zjetlist]):
 			folders = []
-			for algo, corr, folder in zip([plotData.plotdict[i] for i in zjetlist]):
+			for algo, corr, folder in zip(*[plotData.plotdict[i] for i in zjetlist]):
 				folders.append("%s_%s%s" % (folder, algo, corr))
+		plotData.plotdict['folders'] = folders
+
+		super(InputRootZJet, self).prepare_args(parser, plotData)
 
 
 	def auto_detect_type_and_modify_weight(self, weight, root_files, plotData,
