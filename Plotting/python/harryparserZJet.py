a380a06864da5052493f29875e51571c98d7c94f
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-02-04 10:28:32 +0100

    --debug option

--- Plotting/python/harryparserZJet.py
+++ Plotting/python/harryparserZJet.py
@@ -17,3 +17,12 @@ class HarryParserZJet(harryparser.HarryParser):
                           help="""Name of the plot. x-/y-expressions and output filename 
                           are constructed from this, if not explicitly given. 
                           Default is '%(default)s'.""")
+
+		self.add_argument("--debug", action='store_true', help="short for --log-level debug")
+
+	def parse_known_args(self, args=None, namespace=None):
+		known_args, unknown_args = super(HarryParserZJet, self).parse_known_args(args=args, namespace=namespace)
+		# set debug output
+		if known_args.debug == True and known_args.log_level == self.get_default('log_level'):
+			known_args.log_level = 'debug'
+		return known_args, unknown_args
