daf22d314c1c0c883875cb0c2a93937dbffe78c1
Joram Berger <joram.berger@cern.ch>
2015-02-14 14:56:52 +0100

    Add scripts to path

--- scripts/ini_excalibur.sh
+++ scripts/ini_excalibur.sh
@@ -11,6 +11,7 @@ export KAPPAPATH=$EXCALIBURPATH/../Kappa
 export KAPPATOOLSPATH=$EXCALIBURPATH/../KappaTools
 export ARTUSPATH=$EXCALIBURPATH/../Artus
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ARTUSPATH:$KAPPAPATH/lib:$KAPPATOOLSPATH/lib:$BOOSTPATH/lib
+export PATH=$PATH:$EXCALIBURPATH/scripts
 
 export USERPC=`who am i | sed 's/.*(\([^]]*\)).*/\1/g'`
 
