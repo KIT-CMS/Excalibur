106fb165da8a2b53362edc849b430383c2f5047a
Joram Berger <joram.berger@cern.ch>
2015-02-14 13:05:10 +0100

    Use a test file that other users than Dominik can use
    This is now a Kappa 2 file

--- cfg/cfg/mc.py
+++ cfg/cfg/mc.py
@@ -6,7 +6,7 @@ def config():
     cfg = base.getConfig('mc', 2012, 'mm')
     cfg["InputFiles"] = base.setInputFiles(
         #ekppath="root://cms-xrd-global.cern.ch//store/user/tmuller/higgs-kit/skimming/2014-07-30-full_skim/DYJetsToLL_M_50_madgraph_8TeV/kappa_DYJetsToLL_M_50_madgraph_8TeV_1000.root",
-        ekppath="~/home/CMSSW_6_2_3/src/kappa_DYJetsToLL_M_50_madgraph_8TeV_1000.root",
+        ekppath="/storage/6/berger/kappatest/skim_kappa2_data.root",
         nafpath=""
     )
     cfg = base.expand(cfg, ['all', 'zcuts', 'incut'])
