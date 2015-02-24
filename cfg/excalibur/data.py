044436e9bcb7c44f70f8d2a751a106fb6dbc8998
Georg Fleig <gfleig@ekp.uni-karlsruhe.de>
2015-02-24 17:06:49 +0100

    New test file for data

---
+++ cfg/excalibur/data.py
@@ -0,0 +1,13 @@
+import ZJetConfigBase as base
+import ZJetWrapper
+
+
+def config():
+    cfg = base.getConfig('data', 2012, 'mm')
+    cfg["InputFiles"] = base.setInputFiles(
+        #ekppath="root://cms-xrd-global.cern.ch//store/user/tmuller/higgs-kit/skimming/2014-07-30-full_skim/DYJetsToLL_M_50_madgraph_8TeV/kappa_DYJetsToLL_M_50_madgraph_8TeV_1000.root",
+        ekppath="/storage/6/berger/kappatest/skim_kappa2_data.root",
+        nafpath=""
+    )
+    cfg = base.expand(cfg, ['nocuts', 'zcuts', 'finalcuts'])
+    return cfg
