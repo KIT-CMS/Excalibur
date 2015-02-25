f4d9d7900f0fc7ea90a13fe38d3cf04559a1a5ec
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-02-25 14:07:44 +0100

    collection module for dpg plots

---
+++ Plotting/data/python-configs/dpg2015.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import zee_bkgrs
+
+def dpg_dominik():
+	"""Function to create all plots for Dominiks DPG presentation."""
+	zee_bkrgrs.zee_bkrgrs()
+
+
+if __name__ == '__main__':
+	dpg_dominik()
