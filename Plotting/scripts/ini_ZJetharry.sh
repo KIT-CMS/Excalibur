546dde0affe24b093ea086c2fd5eb5facee9b19d
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-02-25 14:57:37 +0100

    add path of python plot configs to PYTHONPATH

--- Plotting/scripts/ini_ZJetharry.sh
+++ Plotting/scripts/ini_ZJetharry.sh
@@ -3,3 +3,5 @@ source $ARTUSPATH/HarryPlotter/scripts/ini_harry.sh
 
 export PYTHONCONFIGS=$EXCALIBURPATH/Plotting/data/python-configs
 export JSONCONFIGS=$EXCALIBURPATH/Plotting/data/json-configs
+
+export PYTHONPATH=$PYTHONCONFIGS:$PYTHONPATH
