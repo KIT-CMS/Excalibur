d829e6d99062b671f34a22cb72e47db111c454d5
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-03-08 18:22:23 +0100

    move standalone function to zjet ini

--- scripts/ini_excalibur.sh
+++ scripts/ini_excalibur.sh
@@ -21,28 +21,3 @@ if [ $USER = "dhaitz" ]; then
     export EXCALIBUR_WORK=/storage/a/dhaitz/zjet
 fi
 
-
-# This function creates a folder with links to python directories, like SCRAM
-# TODO enable merlin standalone usage without reinventing SCRAM ...
-standalone_merlin(){
-    export PYTHONLINKDIR=$EXCALIBURPATH/../python-links
-    export PYTHONPATH=$PYTHONLINKDIR:$PYTHONPATH
-    export PATH=$EXCALIBURPATH/Plotting/scripts:$PATH
-
-    for j in Artus Excalibur; do
-        # base dirs
-        mkdir -p $PYTHONLINKDIR/$j
-        touch $PYTHONLINKDIR/$j/__init__.py
-        for i in `ls -d $(dirname $EXCALIBURPATH)/$j/*/python/`; do
-            # create links to python dirs
-            if [ ! -d $PYTHONLINKDIR/$j/$(basename $(dirname $i)) ]; then
-                ln -s $(dirname $i)/python $PYTHONLINKDIR/$j/$(basename $(dirname $i))
-            fi
-            # create __init__.py in dirs and subdirs
-            for k in `find $PYTHONLINKDIR/$j/*/ -type d`; do
-                touch $k/__init__.py
-            done
-        done
-    done
-    source $EXCALIBURPATH/Plotting/scripts/ini_ZJetharry.sh
-}
