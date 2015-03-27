f59492034efcb0f981d1792c87fdf182ef47886a
Georg Fleig <gfleig@ekp.uni-karlsruhe.de>
2015-03-27 16:14:09 +0100

    Added easy access to valid/corrected jets

--- src/ZJetProduct.h
+++ src/ZJetProduct.h
@@ -3,32 +3,32 @@
 #include "Artus/KappaAnalysis/interface/KappaProduct.h"
 #include "Artus/Utility/interface/SafeMap.h"
 
+#include <boost/ptr_container/ptr_map.hpp>
+
+/**
+   \brief Container class for everything that can be produced in pipeline.
+
+   Defines any outcome that could be produced by a KappaProducer during a common analysis chain in a 
+   given KappaPipeline. Via the PipelineRunner the KappaProduct all extra products in the analysis 
+   chain will be passed on to subsequent Producers, Filters and Consumers.
+*/
+
 class ZJetProduct : public KappaProduct
 {
   public:
 	ZJetProduct() : KappaProduct(){};
 
+	// added by ZProducer
 	IMPL_PROPERTY(bool, ValidZ)
 	IMPL_PROPERTY(KLV, Z)
+	
+	// added by ZJetValidJetsProducer
+	//mutable std::map<std::string, std::vector<KJet*> > m_validZJets;
+	//mutable std::map<std::string, std::vector<KJet*> > m_invalidZJets;
+	
+	// added by ZJetCorrectionsProducer, shared pointers are necessary to keep the jets in the product after creation
+	mutable std::map<std::string, std::vector<std::shared_ptr<KJet> > > m_correctedZJets;
 
-	KJet* GetLeadingJet(std::string const& algoname) const
-	{
-		// if (std::string::npos == algoname.find("L1"))
-		return static_cast<KJet*>(m_validJets.at(0));
-		// else
-		//	return SafeMap::Get(m_validjets, algoname).at(0);
-	}
-
-	KJet* GetSecondJet(std::string const& algoname) const
-	{
-		// if (std::string::npos == algoname.find("L1"))
-		//{
-		return static_cast<KJet*>(m_validJets.at(1));
-		//}
-		// else if (m_validjets.at(algoname).size() < 2)
-		//{
-		// return SafeMap::Get(m_validjets, algoname).at(1);
-	}
 
 	double GetMPF(const KLV* met) const
 	{
@@ -39,5 +39,64 @@ class ZJetProduct : public KappaProduct
 		                  GetRefZ().p4.Py() * GetRefZ().p4.Py();
 
 		return 1.0f + scalPtEt / scalPtSq;
+	}
+
+	// Access to valid jets
+	unsigned int GetValidJetCount(ZJetSettings const& settings,
+								  ZJetEvent const& event,
+								  std::string corrLevel) const
+	{
+		// Gen jets are always valid
+		if (corrLevel == "Gen") {
+			return event.m_genJets->size();
+		}
+		// Uncorrected valid jet
+		else if (corrLevel == "None") {
+			return m_validJets.size();
+		}
+		// Corrected valid jet
+		else {
+			return SafeMap::Get(m_correctedZJets, corrLevel).size();
+		}
+	}
+
+	unsigned int GetValidJetCount(ZJetSettings const& settings,
+								  ZJetEvent const& event) const
+	{
+		return GetValidJetCount(settings, event, settings.GetCorrectionLevel());
+	}
+
+	KLV* GetValidJet(ZJetSettings const& settings,
+					 ZJetEvent const& event,
+					 unsigned int index,
+					 std::string corrLevel) const
+	{
+		assert(GetValidJetCount(settings, event, corrLevel) > index);
+		
+		// Gen jets
+		if (corrLevel == "Gen") {
+			return &(event.m_genJets->at(index));
+		}
+		// Uncorrected valid jet
+		else if (corrLevel == "None") {
+			return static_cast<KLV*>(m_validJets[index]);
+		}
+		// Corrected valid jet
+		else {
+			return (SafeMap::Get(m_correctedZJets, corrLevel)[index]).get();
+		}
+	}
+
+	KLV* GetValidPrimaryJet(ZJetSettings const& settings,
+								ZJetEvent const& event) const
+	{
+		return GetValidJet(settings, event, 0);
+	}
+
+	KLV* GetValidJet(ZJetSettings const& settings,
+						 ZJetEvent const& event,
+						 unsigned int index) const
+	{
+		return GetValidJet(settings, event, index, settings.GetCorrectionLevel());
 	}
 };
