c4e52c7e3d3e9a9434a2fa8c2ff527cc6d751cab
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-04-02 09:29:51 +0200

    rivet-fnlo plot config: norm histos

--- Plotting/data/python-configs/rivet_fastnlo.py
+++ Plotting/data/python-configs/rivet_fastnlo.py
@@ -9,56 +9,59 @@ def rivet_fastnlo(args=None):
 	""" compare rivet with fastnlo and MC-gen"""
 
 	plots = []
-	for quantity, x, xlabel, upper_limit in zip(['pt', 'y'], ["16", "50"], ['zpt', 'abs(zy)'], [100, 3]):
-		for file1, label1, filename_suffix, xx in zip(
-			["/usr/users/dhaitz/home/qcd/work/Rivet_10000.root", "/usr/users/dhaitz/home/artus/Excalibur/plots/genz{}.root".format(quantity)],
-			["Sherpa+Rivet", 'Madgraph+Pythia'],
-			["fnlo", "mg"],
-			[x, 'nick0']
-		):
+	# normalized or not
+	for title, norm_modules, y_label_suffix, filename_norm_suffix in zip(
+		['', "shape comparison"],
+		[[], ["NormalizeToFirstHisto", "NormalizeToUnity"]],
+		['', " (normalized)"],
+		['', '_normalizes'],
+	):
+		for quantity, x, xlabel, upper_limit in zip(['pT', 'y'], ["16", "50"], ['zpt', 'abs(zy)'], [100, 3]):
+			for file1, label1, filename_suffix, xx in zip(
+				["/usr/users/dhaitz/home/qcd/work/Rivet_10000.root", "/usr/users/dhaitz/home/artus/Excalibur/plots/genz{}.root".format(quantity.lower())],
+				["Sherpa+Rivet", 'Madgraph+Pythia'],
+				["fnlo", "mg"],
+				[x, 'nick0']
+			):
+				d = {
+					"analysis_modules": norm_modules + [
+						"Ratio"
+					],
+					"files": [
+						"/usr/users/dhaitz/home/qcd/work/fnlo-results/fnlo_{}Z.root".format(quantity),
+						 file1,
+					],
+					"folders": [
+						""
+					],
+					"labels": [
+						"Sherpa+fastNLO",
+						label1,
+					],
+					"markers": [
+						"o",
+						"fill",
+						"o"
+					],
+					"filename": quantity + "_riv-" + filename_suffix + filename_norm_suffix,
+					"title": title,
+					"x_expressions": [
+						"0",
+						xx,
+					],
+					"x_label": xlabel,
+					"x_lims": [
+						0.0,
+						upper_limit
+					],
+					"y_label": "Events" + y_label_suffix,
+					"y_subplot_lims": [
+						0.5,
+						1.5
+					]
+				}
+				plots.append(d)
 
-			d = {
-				"analysis_modules": [
-					"NormalizeToFirstHisto", 
-					"NormalizeToUnity", 
-					"Ratio"
-				], 
-				"files": [
-					"/usr/users/dhaitz/home/qcd/work/fnlo_{}.root".format(quantity),
-					 file1,
-				], 
-				"folders": [
-					""
-				], 
-				"labels": [
-					"Sherpa+fastNLO",
-					label1,
-				], 
-				"markers": [
-					"o", 
-					"fill", 
-					"o"
-				],
-				"filename": quantity + "_riv-" + filename_suffix,
-				"title": "shape comparison", 
-				"x_expressions": [
-					"0",
-					xx,
-				], 
-				"x_label": xlabel, 
-				"x_lims": [
-					0.0, 
-					upper_limit
-				], 
-				"y_label": "Events (normalized)", 
-				"y_subplot_lims": [
-					0.5, 
-					1.5
-				]
-			}
-
-
-			plots.append(d)
 	plotscript.plotscript(plots, args)
 
 if __name__ == '__main__':
