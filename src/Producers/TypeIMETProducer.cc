7866f78cb609566d34f1be1fc8313577f344475a
Georg Fleig <gfleig@ekp.uni-karlsruhe.de>
2015-04-08 16:03:45 +0200

    Code cleanup and sumET bugfix

--- src/Producers/TypeIMETProducer.cc
+++ src/Producers/TypeIMETProducer.cc
@@ -22,55 +22,56 @@ void TypeIMETProducer::Init(ZJetSettings const& settings)
 	// Flavor based corrections
 	if (settings.GetFlavourCorrections())
 	{
-		m_corrLevels.emplace_back("L1L2L3L5Algo");
-		m_corrLevels.emplace_back("L1L2L3L5Phys");
+		m_corrLevels.emplace_back("L1L2L3L5q");
+		m_corrLevels.emplace_back("L1L2L3L5g");
+		m_corrLevels.emplace_back("L1L2L3L5c");
+		m_corrLevels.emplace_back("L1L2L3L5b");
 	}
 }
 
 void TypeIMETProducer::Produce(ZJetEvent const& event, ZJetProduct& product,
                                ZJetSettings const& settings) const
 {
-	KMET* rawmet;
-	float sumEt_correction = 0;
-
-	rawmet = event.m_met;
-
-	// Iterate over the jet collection and sum up the differences between L1L2L3(Res) and L1
+	// Iterate over the jet collection
 	for (unsigned int corrLevelIndex = 0; corrLevelIndex < m_corrLevels.size(); corrLevelIndex++)
 	{
 		KLV correction;
+		float sumEtCorrection = 0;
+		
+		// Iterate over individual jets and sum up the differences between L1L2L3(Res) and L1
 		for (unsigned int jetIndex = 0; jetIndex < product.m_correctedZJets.at(m_corrLevels[corrLevelIndex]).size(); ++jetIndex)
 		{
-			KLV* corrjet = (SafeMap::Get(product.m_correctedZJets, m_corrLevels[corrLevelIndex]).at(jetIndex)).get();
+			KLV* corrJet = (SafeMap::Get(product.m_correctedZJets, m_corrLevels[corrLevelIndex]).at(jetIndex)).get();
 
-			if (corrjet->p4.Pt() > settings.GetJetPtMin())
+			// Only consider jets with Pt above 10 GeV
+			if (corrJet->p4.Pt() > settings.GetJetPtMin())
 			{
-				KLV* l1jet = (SafeMap::Get(product.m_correctedZJets, m_l1Corr).at(jetIndex)).get();
-				correction.p4 +=  l1jet->p4 - corrjet->p4;
-				sumEt_correction += correction.p4.Pt();
+				KLV* l1Jet = (SafeMap::Get(product.m_correctedZJets, m_l1Corr).at(jetIndex)).get();
+				correction.p4 +=  l1Jet->p4 - corrJet->p4;
+				sumEtCorrection += correction.p4.Pt();
 			}
 		}
-
-		KMET corrmet = * rawmet;
-		corrmet.p4 += correction.p4;
+		KMET corrMET = *event.m_met;
+		corrMET.p4 += correction.p4;
 
 		// Eta of MET is always zero
-		corrmet.p4.SetEta(0.0f);
-		corrmet.sumEt = rawmet->sumEt + sumEt_correction;
+		corrMET.p4.SetEta(0.0f);
+		corrMET.sumEt = event.m_met->sumEt + sumEtCorrection;
 
 		// Apply MET-phi-corrections
 		if (settings.GetEnableMetPhiCorrection())
 		{
-			double px = corrmet.p4.Px();
-			double py = corrmet.p4.Py();
+			double px = corrMET.p4.Px();
+			double py = corrMET.p4.Py();
 
 			px = px - (m_metPhiCorrectionParameters.at(0) + m_metPhiCorrectionParameters.at(1) * event.m_vertexSummary->nVertices);
 			py = py - (m_metPhiCorrectionParameters.at(2) + m_metPhiCorrectionParameters.at(3) * event.m_vertexSummary->nVertices);
 
-			corrmet.p4.SetPt(sqrt(px * px + py * py));
-			corrmet.p4.SetPhi(atan2(py, px));
+			corrMET.p4.SetPt(sqrt(px * px + py * py));
+			corrMET.p4.SetPhi(atan2(py, px));
 		}
-		product.m_corrMET[m_corrLevels[corrLevelIndex]] = corrmet;
+		
+		// Store corrected MET in product
+		product.m_corrMET[m_corrLevels[corrLevelIndex]] = corrMET;
 	}
-
 }
