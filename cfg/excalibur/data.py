22d3e9b1dae65486cc189c2a9c5a73dbb82637a9
Georg Fleig <gfleig@ekp.uni-karlsruhe.de>
2015-04-15 18:15:13 +0200

    data.py, mc.py: Cleaned list of input files

--- cfg/excalibur/data.py
+++ cfg/excalibur/data.py
@@ -4,9 +4,9 @@ import ZJetConfigBase as base
 def config():
     cfg = base.getConfig('data', 2012, 'mm')
     cfg["InputFiles"] = base.setInputFiles(
-        ekppath="/storage/6/berger/kappatest/skim_kappa2_data.root", # DATA, AK5PFTaggedJets missing
-        nafpath=""
+        ekppath="/storage/a/dhaitz/skims/2015-04-13_DoubleMu_Run2012_22Jan2013_8TeV/*.root",
+        nafpath="/pnfs/desy.de/cms/tier2/store/user/dhaitz/2015-04-13_DoubleMu_Run2012_22Jan2013_8TeV/"
     )
-    cfg = base.expand(cfg, ['nocuts', 'zcuts', 'finalcuts'])
+    cfg = base.expand(cfg, ['finalcuts'])
     print cfg
     return cfg
