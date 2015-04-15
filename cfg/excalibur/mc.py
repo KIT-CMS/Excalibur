daa442dd2c405a5c22aa63dfe92419838f4be960
Georg Fleig <gfleig@ekp.uni-karlsruhe.de>
2015-04-15 10:21:40 +0200

    mc.py: updated

--- cfg/excalibur/mc.py
+++ cfg/excalibur/mc.py
@@ -5,10 +5,11 @@ def config():
     cfg = base.getConfig('mc', 2012, 'mm')
     cfg["InputFiles"] = base.setInputFiles(
         #ekppath="/storage/a/berger/kappatest/output/higgsTauTau_kSkimming_cfg.root", # MC, 100 events
-        ekppath="/storage/6/berger/testfiles/kappa_DYJetsToLL_M_50_madgraph_8TeV_5147.root", # MC, 20000 events
+        #ekppath="/storage/6/berger/testfiles/kappa_DYJetsToLL_M_50_madgraph_8TeV_5147.root", # MC, 20000 events
         #ekppath="/storage/a/dhaitz/skims/test/DYJetsToLL_M_50_madgraph_8TeV_kappa2.root", # MC, 1000 events
         #ekppath="/home/berger/runtests/newmaster/2015-03-27_skim_kappa2_7_2_2/CMSSW_7_2_2/src/kappatuple.root", # MC, new skimmed file with different met name
-        nafpath="/pnfs/desy.de/cms/tier2/store/user/dhaitz/2015-04-08_DYJetsToLL_M_50_madgraph_8TeV/"
+        ekppath="/storage/a/dhaitz/skims/2015-04-08_DYJetsToLL_M_50_madgraph_8TeV/*.root",
+        nafpath="/pnfs/desy.de/cms/tier2/store/user/dhaitz/2015-04-08_DYJetsToLL_M_50_madgraph_8TeV/",
     )
     cfg = base.expand(cfg, ['nocuts', 'zcuts', 'finalcuts'])
     print cfg
