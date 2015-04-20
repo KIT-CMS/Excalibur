9e6bfd9495c37d68493122865812e3af0f098569
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-04-20 16:51:57 +0200

    added excalibur cfg for 7.4.0 data2012 rereco

---
+++ cfg/excalibur/data740.py
@@ -0,0 +1,11 @@
+import ZJetConfigBase as base
+import data
+
+def config():
+    cfg = data.config()
+    cfg["InputFiles"] = base.setInputFiles(
+        ekppath="/storage/a/dhaitz/skims/2015-04-16_DoubleMu_Run2012_740pre9ROOT6_8TeV/*.root",
+        nafpath="/pnfs/desy.de/cms/tier2/store/user/dhaitz/2015-04-16_DoubleMu_Run2012_740pre9ROOT6_8TeV/*.root",
+    )
+    cfg['Met'] = 'pfChMet'
+    return cfg
