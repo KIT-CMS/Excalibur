bd59ba04b46face95fe0dc899718cc065898729b
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-04-20 16:48:15 +0200

    ini_excalibur.sh: add config folder to PYTHONPATH

--- scripts/ini_excalibur.sh
+++ scripts/ini_excalibur.sh
@@ -13,7 +13,7 @@ BOOSTVER=${BOOSTPATH%-*}
 export BOOSTLIB=${BOOSTPATH}/lib/libboost_regex.so.${BOOSTVER/*\//}
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ARTUSPATH:$BOOSTPATH/lib
 export PATH=$PATH:$EXCALIBURPATH/scripts
-export PYTHONPATH=$PYTHONPATH:$EXCALIBURPATH/python
+export PYTHONPATH=$PYTHONPATH:$EXCALIBURPATH/python:$EXCALIBURPATH/cfg/excalibur
 export USERPC=`who am i | sed 's/.*(\([^]]*\)).*/\1/g'`
 
 
