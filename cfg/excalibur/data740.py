9c7f5e898c1a9131b1bf520a093641a5a6f77900
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-04-20 17:00:34 +0200

    remove hlt filter for data740 cfg

--- cfg/excalibur/data740.py
+++ cfg/excalibur/data740.py
@@ -8,4 +8,5 @@ def config():
         nafpath="/pnfs/desy.de/cms/tier2/store/user/dhaitz/2015-04-16_DoubleMu_Run2012_740pre9ROOT6_8TeV/*.root",
     )
     cfg['Met'] = 'pfChMet'
+    cfg['Processors'].remove("filter:HltFilter")
     return cfg
