c2e03dd57a4f45d1fcca29b852f4deb82990551e
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-04-22 08:23:11 +0200

    New skim location for 740 data

--- cfg/excalibur/data740.py
+++ cfg/excalibur/data740.py
@@ -4,9 +4,8 @@ import data
 def config():
     cfg = data.config()
     cfg["InputFiles"] = base.setInputFiles(
-        ekppath="/storage/a/dhaitz/skims/2015-04-16_DoubleMu_Run2012_740pre9ROOT6_8TeV/*.root",
-        nafpath="/pnfs/desy.de/cms/tier2/store/user/dhaitz/2015-04-16_DoubleMu_Run2012_740pre9ROOT6_8TeV/*.root",
+        ekppath="/storage/a/dhaitz/skims/2015-04-21_DoubleMu_Run2012_740pre9ROOT6_8TeV/*.root",
+        nafpath="/pnfs/desy.de/cms/tier2/store/user/dhaitz/2015-04-21_DoubleMu_Run2012_740pre9ROOT6_8TeV/*.root",
     )
-    cfg['Met'] = 'pfChMet'
     cfg['Processors'].remove("filter:HltFilter")
     return cfg
