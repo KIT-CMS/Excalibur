3b5db076c7abdd75ab48c52f36738c6f0506804d
Georg Fleig <gfleig@ekp.uni-karlsruhe.de>
2015-04-23 14:52:02 +0200

    data.py: updated for current 74/53 study (removed hlt)

--- cfg/excalibur/data.py
+++ cfg/excalibur/data.py
@@ -4,9 +4,11 @@ import ZJetConfigBase as base
 def config():
     cfg = base.getConfig('data', 2012, 'mm')
     cfg["InputFiles"] = base.setInputFiles(
-        ekppath="/storage/a/dhaitz/skims/2015-04-13_DoubleMu_Run2012_22Jan2013_8TeV/*.root",
+        #ekppath="/storage/a/dhaitz/skims/2015-04-13_DoubleMu_Run2012_22Jan2013_8TeV/*.root",
+        ekppath="/storage/a/dhaitz/skims/2015-04-13_DoubleMu_Run2012_22Jan2013_8TeV/kappa_DoubleMuParked_Run2012D_22Jan2013_8TeV_*.root",
         nafpath="/pnfs/desy.de/cms/tier2/store/user/dhaitz/2015-04-13_DoubleMu_Run2012_22Jan2013_8TeV/*.root",
     )
     cfg = base.expand(cfg, ['nocuts', 'zcuts', 'finalcuts'], ['L1L2L3', 'L1L2L3Res'])
+    cfg['Processors'].remove("filter:HltFilter")
 
     return cfg
