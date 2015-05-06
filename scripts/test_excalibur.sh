8d4fdf41138681e0e3e824dea9520e69bbede92d
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-05-06 16:11:13 +0200

    test_excalibur.sh: -a option

--- scripts/test_excalibur.sh
+++ scripts/test_excalibur.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# Usage: test_excalibur.sh [arg=data]   arg can be data or mc
+# Usage: test_excalibur.sh [arg=data][-a]   arg can be data or mc
 
 TYPE=${1:-data}
 $EXCALIBURPATH/scripts/artus $EXCALIBURPATH/test/$TYPE.py.json
 
 echo -e "\n\nChecking reference file and new output"
-compareRootFiles.py $TYPE.root $EXCALIBURPATH/test/$TYPE.root
+compareRootFiles.py $TYPE.root $EXCALIBURPATH/test/$TYPE.root $2
