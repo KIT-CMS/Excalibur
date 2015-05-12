25c118dbf6867f1e9cf31f1f56d2c2690462c2e3
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-05-12 10:41:35 +0200

    Updated rivet-fastnlo plots

--- Plotting/plot-configs/python-configs/rivet_fastnlo.py
+++ Plotting/plot-configs/python-configs/rivet_fastnlo.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from itertools import combinations
+
 import Artus.HarryPlotter.harry as harry
 import Excalibur.Plotting.harryinterface as harryinterface
 
@@ -9,41 +11,42 @@ def rivet_fastnlo(args=None):
 	""" compare rivet with fastnlo and MC-gen"""
 
 	plots = []
-	# normalized or not
+	# normalized or not:
 	for title, norm_modules, y_label_suffix, filename_norm_suffix in zip(
 		['', "shape comparison"],
-		[[], ["NormalizeToFirstHisto", "NormalizeToUnity"]],
+		[[], ["NormalizeToUnity"]],
 		['', " (normalized)"],
 		['', '_normalizes'],
 	):
+		# pT or y:
 		for quantity, x, xlabel, upper_limit in zip(['pT', 'y'], ["d01-x01-y01", "d02-x01-y01"], ['zpt', 'abs(zy)'], [100, 3]):
-			for file1, label1, filename_suffix, path, weight in zip(
-				["/usr/users/dhaitz/home/qcd/work/rivet-results/Rivet.root", "/usr/users/dhaitz/home/artus/Excalibur/plots/genz{}.root".format(quantity.lower())],
-				["Sherpa+Rivet", 'Madgraph+Pythia'],
-				["fnlo", "mg"],
-				[x, '4'],
-				['1', '19.789'],
+			# all combinations of the 
+			for files, labels, filename_suffixes, paths, weights in zip(
+				combinations([
+					"/usr/users/dhaitz/home/qcd/sherivf/rivet-results/Rivet.root",
+					"/usr/users/dhaitz/home/artus/Excalibur/plots/genz{0}.root".format(quantity.lower()),
+					"/usr/users/dhaitz/home/qcd/sherivf/fnlo-results/fnlo_{0}Z.root".format(quantity)], 2),
+				combinations(["Sherpa+Rivet", "Madgraph+Pythia", "Sherpa+fastNLO"], 2),
+				combinations(["Riv", "MG", "Fnlo"], 2),
+				combinations([x, '4', '0'], 2),
+				combinations(['1', '19.789', '1'], 2)
 			):
 				d = {
 					"analysis_modules": norm_modules + ["Ratio"],
-					"files": [
-						"/usr/users/dhaitz/home/qcd/work/fnlo-results/fnlo_{}Z.root".format(quantity),
-						 file1,
-					],
+					"files": files,
 					"folders": [""],
-					"labels": ["Sherpa+fastNLO", label1],
+					"labels": labels,
 					"markers": ["o", "fill","o"],
-					"filename": quantity + "_riv-" + filename_suffix + filename_norm_suffix,
+					"filename": quantity + "_riv-" + "_".join(filename_suffixes) + filename_norm_suffix,
 					"title": title,
-					"scale_factors": ["1", weight],
-					"x_expressions": ["0", path],
+					"scale_factors": weights,
+					"x_expressions": paths,
 					"x_label": xlabel,
 					"x_lims": [0.0, upper_limit],
 					"y_label": "Events" + y_label_suffix,
 					"y_subplot_lims": [0.5, 1.5],
 				}
 				plots.append(d)
-
 	harryinterface.harry_interface(plots, args)
 
 
