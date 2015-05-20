9a63a29adbcde515b63a7f3c238d905d86093880
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-05-20 19:48:44 +0200

    fix data740 cfg (no tagger, no hlt)

--- cfg/excalibur/data740.py
+++ cfg/excalibur/data740.py
@@ -2,10 +2,14 @@ import ZJetConfigBase as base
 import data
 
 def config():
-    cfg = data.config()
-    cfg["InputFiles"] = base.setInputFiles(
-        ekppath="/storage/a/dhaitz/skims/2015-04-21_DoubleMu_Run2012_740pre9ROOT6_8TeV/*.root",
-        nafpath="/pnfs/desy.de/cms/tier2/store/user/dhaitz/2015-04-21_DoubleMu_Run2012_740pre9ROOT6_8TeV/*.root",
-    )
+	cfg = data.config()
+	cfg["InputFiles"] = base.setInputFiles(
+		ekppath="/storage/a/dhaitz/skims/2015-04-21_DoubleMu_Run2012_740pre9ROOT6_8TeV/*.root",
+		nafpath="/pnfs/desy.de/cms/tier2/store/user/dhaitz/2015-04-21_DoubleMu_Run2012_740pre9ROOT6_8TeV/*.root",
+	)
+	cfg['Processors'].remove("filter:HltFilter")
+	for pipeline in cfg['Pipelines']:
+		for tag in ['jet1btag', 'jet1qgtag']:
+			cfg['Pipelines'][pipeline]['Quantities'].remove(tag)
 
-    return cfg
+	return cfg
