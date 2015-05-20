4f7471cf64afb51d74bda7bddca238743dfe62d0
Georg Fleig <gfleig@ekp.uni-karlsruhe.de>
2015-05-20 11:24:27 +0200

    data.py: Hlt filter is not a problem for 53X data

--- cfg/excalibur/data.py
+++ cfg/excalibur/data.py
@@ -8,6 +8,5 @@ def config():
         nafpath="/pnfs/desy.de/cms/tier2/store/user/dhaitz/2015-05-18_DoubleMu_Run2012_22Jan2013_8TeV/*.root",
     )
     cfg = base.expand(cfg, ['nocuts', 'zcuts', 'noalphanoetacuts', 'noalphacuts', 'noetacuts', 'finalcuts'], ['L1L2L3', 'L1L2L3Res'])
-    cfg['Processors'].remove("filter:HltFilter")
 
     return cfg
