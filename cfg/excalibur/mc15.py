d1468a4299a22c9ddbb05ab1ff495ce8fadca1cc
Georg Fleig <gfleig@ekp.uni-karlsruhe.de>
2015-05-21 13:28:48 +0200

    mc15.py: removed unavailable quantities

--- cfg/excalibur/mc15.py
+++ cfg/excalibur/mc15.py
@@ -2,11 +2,17 @@ import ZJetConfigBase as base
 
 
 def config():
-    cfg = base.getConfig('mc', 2015, 'mm')
-    cfg["InputFiles"] = base.setInputFiles(
-        ekppath="/storage/a/dhaitz/skims/2015-05-18_DYJetsToLL_M_50_madgraph_13TeV/*.root",
-        nafpath="/pnfs/desy.de/cms/tier2/store/user/dhaitz/2015-05-18_DYJetsToLL_M_50_madgraph_13TeV/*.root",
-    )
-    cfg = base.expand(cfg, ['nocuts', 'zcuts', 'noalphanoetacuts', 'noalphacuts', 'noetacuts', 'finalcuts'], ['L1L2L3'])
+	cfg = base.getConfig('mc', 2015, 'mm')
+	cfg["InputFiles"] = base.setInputFiles(
+		ekppath="/storage/a/dhaitz/skims/2015-05-18_DYJetsToLL_M_50_madgraph_13TeV/*.root",
+		nafpath="/pnfs/desy.de/cms/tier2/store/user/dhaitz/2015-05-18_DYJetsToLL_M_50_madgraph_13TeV/*.root",
+	)
+	cfg = base.expand(cfg, ['nocuts', 'zcuts', 'noalphanoetacuts', 'noalphacuts', 'noetacuts', 'finalcuts'], ['L1L2L3'])
+	
+	for pipeline in cfg['Pipelines']:
+		for tag in ['jet1btag', 'jet1qgtag']:
+			cfg['Pipelines'][pipeline]['Quantities'].remove(tag)
 
-    return cfg
+	cfg['JetMatchingAlgorithm'] = 'algorithmic'
+
+	return cfg
