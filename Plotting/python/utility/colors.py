d37ca45fecdebc05e4f79b62fb4bca64f94d6a74
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-05-22 11:09:04 +0200

    fix comments and formatting

--- Plotting/python/utility/colors.py
+++ Plotting/python/utility/colors.py
@@ -5,11 +5,11 @@
 """
 
 histo_colors = {
-	'blue': '#7293cb',  # light blue
+	'blue'  : '#7293cb',  # light blue
 	'yellow': '#FAA75B',  # mustard yellow / orange
-	'green': '#68A55A',  # green
-	'brown': '#CE7058',  # brown
-	'violet': '#9E67AB',  # violet
-	'grey': '#737373',  # grey
-	'red': '#CE3B3E',  # red
+	'green' : '#68A55A',
+	'brown' : '#CE7058',
+	'violet': '#9E67AB',
+	'grey'  : '#737373',
+	'red'   : '#CE3B3E',  # light red
 }
