81526f7cf491378b141eb09ed36bc5745ffe82c8
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-05-22 09:47:22 +0200

    sortedflavour and abssortedflavour

--- Plotting/python/utility/quantities.py
+++ Plotting/python/utility/quantities.py
@@ -8,5 +8,14 @@ quantities_dict = {
 	'ptbalance': '(jet1pt/zpt)',
 	'deltaphizjet1' : '(abs(abs(abs(zphi-jet1phi)-TMath::Pi())-TMath::Pi()))',
 	'deltaphizmet' : '(abs(abs(abs(zphi-metphi)-TMath::Pi())-TMath::Pi()))',
-	'sortedflavour' : 'abs(matchedgenparton1flavour)*(abs(matchedgenparton1flavour)<20)+6*(abs(matchedgenparton1flavour)==21)+0*(abs(matchedgenparton1flavour)==-999)',
+	'sortedflavour' : (
+		'matchedgenparton1flavour*(matchedgenparton1flavour<20 && matchedgenparton1flavour>(-20))'  # quarks: ok
+		+'+6*(matchedgenparton1flavour==21)'  # gluons: 21 -> 6
+		+'+0*(matchedgenparton1flavour==-999)'  # undef ->0
+	),
+	'abssortedflavour' : (
+		'abs(matchedgenparton1flavour)*(abs(matchedgenparton1flavour)<20)'  # quarks: ok
+		+'+6*(abs(matchedgenparton1flavour)==21)'  # gluons: 21 -> 6
+		+'+0*(abs(matchedgenparton1flavour)==-999)'  # undef ->0
+	),
 }
