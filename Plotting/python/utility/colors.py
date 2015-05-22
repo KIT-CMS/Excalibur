dc9629259f8593abf8fd3b9d9c6cb2eb0a95e1fa
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-05-22 14:27:48 +0200

    color update

--- Plotting/python/utility/colors.py
+++ Plotting/python/utility/colors.py
@@ -10,6 +10,6 @@ histo_colors = {
 	'green' : '#68A55A',
 	'brown' : '#CE7058',
 	'violet': '#9E67AB',
-	'grey'  : '#737373',
-	'red'   : '#CE3B3E',  # light red
+	'grey'  : 'grey',
+	'red'   : '#D35658',  # light red
 }
