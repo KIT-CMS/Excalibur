5ebbc33232536e55c2cbcee425e72f86725fd269
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-06-09 10:31:12 +0200

    New skim for 13 TeV MC

--- cfg/excalibur/mc15.py
+++ cfg/excalibur/mc15.py
@@ -4,8 +4,8 @@ import ZJetConfigBase as base
 def config():
 	cfg = base.getConfig('mc', 2015, 'mm')
 	cfg["InputFiles"] = base.setInputFiles(
-		ekppath="/storage/a/dhaitz/skims/2015-05-18_DYJetsToLL_M_50_madgraph_13TeV/*.root",
-		nafpath="/pnfs/desy.de/cms/tier2/store/user/dhaitz/2015-05-18_DYJetsToLL_M_50_madgraph_13TeV/*.root",
+		ekppath="/storage/a/dhaitz/skims/2015-06-08_DYJetsToLL_M_50_madgraph_13TeV/*.root",
+		nafpath="/pnfs/desy.de/cms/tier2/store/user/dhaitz/2015-06-08_DYJetsToLL_M_50_madgraph_13TeV/*.root",
 	)
 	cfg = base.expand(cfg, ['nocuts', 'zcuts', 'noalphanoetacuts', 'noalphacuts', 'noetacuts', 'finalcuts'], ['L1L2L3'])
 	
