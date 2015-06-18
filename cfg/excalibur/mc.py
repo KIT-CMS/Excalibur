c7162e9dc6271b8325aacda819b951945a317a07
Joram Berger <joram.berger@cern.ch>
2015-06-18 19:34:16 +0200

    Fix up Config

--- cfg/excalibur/mc.py
+++ cfg/excalibur/mc.py
@@ -3,10 +3,11 @@ import ZJetConfigBase as base
 
 def config():
     cfg = base.getConfig('mc', 2012, 'mm')
+    cfg['JetMatchingAlgorithm'] = 'algorithmic'
     cfg["InputFiles"] = base.setInputFiles(
         ekppath="/storage/a/dhaitz/skims/2015-05-16_DYJetsToLL_M_50_madgraph_8TeV/*.root",
         nafpath="/pnfs/desy.de/cms/tier2/store/user/dhaitz/2015-05-16_DYJetsToLL_M_50_madgraph_8TeV/*.root",
     )
-    cfg = base.expand(cfg, ['nocuts', 'zcuts', 'noalphanoetacuts', 'noalphacuts', 'noetacuts', 'finalcuts'], ['L1L2L3'])
+    cfg = base.expand(cfg, ['nocuts', 'zcuts', 'noalphanoetacuts', 'noalphacuts', 'noetacuts', 'finalcuts'], ['None', 'L1', 'L1L2L3'])
 
     return cfg
