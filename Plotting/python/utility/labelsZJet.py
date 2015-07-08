a4e779b2540fbb9fccd6920e8770b4fe9fa0b295
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-07-08 15:49:01 +0200

    more labels

--- Plotting/python/utility/labelsZJet.py
+++ Plotting/python/utility/labelsZJet.py
@@ -44,6 +44,8 @@ class LabelsDictZJet(labels.LabelsDict):
 			'jet1pt': '$\mathit{p}_{T}^{Leading Jet}$ / GeV',
 			'jet1btag': 'CSV b-Tag',
 			'jet1qgtag': 'QG Likelihood',
+			'jeteta': '$\mathit{\eta}_{Jet}$',
+			'jetpt': '$\mathit{p}_{T}^{Jet}$ / GeV',
 			'(jet1nhf*jet1pt)': 'Jet Neutral Hadron Energy / GeV',
 			'(jet1chf*jet1pt)': 'Jet Charged Hadron Energy / GeV',
 			'(jet1mf*jet1pt)': 'Jet Muon Energy / GeV',
