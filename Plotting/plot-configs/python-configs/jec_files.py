b89fa084dc022594b2660a702d63928a0b651aa0
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-07-09 16:43:00 +0200

    jec_files.py: some formatting and comments

--- Plotting/plot-configs/python-configs/jec_files.py
+++ Plotting/plot-configs/python-configs/jec_files.py
@@ -8,24 +8,22 @@ import glob
 import copy
 
 def jec_files(args=None, additional_dictionary=None):
+	"""make plots for L1,RC,L1L2L3,Res files in a directory.
+
+	Usage: merlin.py --py jec_files --jec-dir data/jec/Winter14_V8"""
 	plots = []
 
 	parser = argparse.ArgumentParser()
-	parser.add_argument('--jec-dir', type=str,
-		default="data/jec/Winter14_V6",
-		help="path to jec directory, e.g. data/jec/Winter14_V8"
-	)
-	parser.add_argument('--jec-algo', type=str,
-		default="AK5PFchs",
-		help="algo"
-	)
+	parser.add_argument('--jec-dir', type=str, default="data/jec/Winter14_V6",
+		help="path to jec directory, e.g. data/jec/Winter14_V8")
+	parser.add_argument('--jec-algo', type=str, default="AK5PFchs", help="algo")
 	
-
 	if args is None:
 		known_args, args = parser.parse_known_args()
 	else:
 		known_args, args = parser.parse_known_args(args)
 
+	# get the list of files from the path
 	files = glob.glob(known_args.jec_dir+"/*.txt")
 	files = [file for file in files if known_args.jec_algo in file]
 
@@ -34,14 +32,12 @@ def jec_files(args=None, additional_dictionary=None):
 		"RC": "L1RC",
 	}
 
-
 	basedict = {
 		"input_modules": ["InputJEC"],
 		"x_expressions": ["jeteta"],
 		"y_bins": ["100,0,100"],
 		"y_expressions": ["jetpt"],
 		"z_label": "JEC Correction Factor",
-
 		"colormap": "seismic",
 		"z_lims": [0.0, 2.0]
 	}
@@ -58,7 +54,5 @@ def jec_files(args=None, additional_dictionary=None):
 				plots.append(d)
 			else:
 				print typ, level, "NOT FOUND:", len(matching_files)
-
-
 
 	harryinterface.harry_interface(plots, args)
