c1479147c18f100c000dd1953d86ee204ca7e76b
Joram Berger <joram.berger@cern.ch>
2015-07-09 11:24:22 +0200

    Also switch back to old naming in data config

--- cfg/excalibur/data.py
+++ cfg/excalibur/data.py
@@ -3,6 +3,7 @@ import ZJetConfigBase as base
 
 def config():
     cfg = base.getConfig('data', 2012, 'mm')
+    base.changeNamingScheme(cfg)
     cfg["InputFiles"] = base.setInputFiles(
         ekppath="/storage/a/dhaitz/skims/2015-05-18_DoubleMu_Run2012_22Jan2013_8TeV/*.root",
         nafpath="/pnfs/desy.de/cms/tier2/store/user/dhaitz/2015-05-18_DoubleMu_Run2012_22Jan2013_8TeV/*.root",
