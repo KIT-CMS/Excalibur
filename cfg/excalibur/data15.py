ac61e2ecfb2f5f4221d9becd0a0b34d9fc1fac69
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-07-15 13:29:42 +0200

    switch to latest skim for 13tev data

--- cfg/excalibur/data15.py
+++ cfg/excalibur/data15.py
@@ -4,7 +4,7 @@ import ZJetConfigBase as base
 def config():
 	cfg = base.getConfig('data', 2015, 'mm')
 	cfg["InputFiles"] = base.setInputFiles(
-		ekppath="/storage/a/mfischer/skims/2015-07-10_MF_AnyMu_2015_746/MIN_MU_COUNT_2-MIN_MU_PT_8.0/DoubleMu_Run2015B_Jul2015_13TeV/*.root",
+		ekppath="/storage/a/mfischer/skims/MF_AnyMu_2015_746/recent/MIN_MU_COUNT_2-MIN_MU_PT_8.0/DoubleMu_Run2015B_Jul2015_13TeV/*.root",
 		#nafpath="/pnfs/desy.de/cms/tier2/store/user/dhaitz/2015-05-18_DoubleMu_Run2012_22Jan2013_8TeV/*.root",
 	)
 	cfg = base.expand(cfg, ['nocuts', 'zcuts', 'noalphanoetacuts', 'noalphacuts', 'noetacuts', 'finalcuts'], ['None', 'L1', 'L1L2L3'])
