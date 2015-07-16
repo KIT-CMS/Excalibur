01b550dfb54b20690cc3d6de8f727c48e67890c0
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-07-16 13:32:25 +0200

    Added small bash function for simple reweighting

--- scripts/ini_merlin.sh
+++ scripts/ini_merlin.sh
@@ -31,3 +31,12 @@ standalone_merlin(){
         done
     done
 }
+
+# get the weights for a simple reweighting in npv. weight string is printed as output
+# Usage: get_weights data.root mc.root
+get_weights(){
+	ALGO=ak4PFJetsCHS
+	QUANTITY=npv
+	BINNING="30,0.5,30.5"
+	echo "(npv=="`merlin.py -x $QUANTITY -i $1 $2 --zjetf nocuts --corr L1L2L3 --algo $ALGO --analy NormalizeToFirstHisto Ratio --nicks-white ratio --log-l debug --x-bins $BINNING --y-lims 0 1 | grep fSumw | cut -d "=" -f 2 | cut -d "," -f 1 | while read i; do echo ")*${i}+(npv=="; done | nl | sed 's/ //g' | sed 's/\t//g' ` | sed 's/ //g' | sed 's/.\{7\}$//'
+}
