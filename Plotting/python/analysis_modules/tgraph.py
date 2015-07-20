eca4f3005ac638372ee97e683d65c88d13b184e8
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-07-20 16:01:07 +0200

    Make ConvertToTGraphErrors module more configurable

--- Plotting/python/analysis_modules/tgraph.py
+++ Plotting/python/analysis_modules/tgraph.py
@@ -12,10 +12,23 @@ import Artus.HarryPlotter.analysisbase as analysisbase
 
 class ConvertToTGraphErrors(analysisbase.AnalysisBase):
 
+	def modify_argument_parser(self, parser, args):
+		super(ConvertToTGraphErrors, self).modify_argument_parser(parser, args)
+
+		self.convert_options = parser.add_argument_group("{} options".format(self.name()))
+		self.convert_options.add_argument(
+				"--convert-nicks", nargs="+", default=None,
+				help="Nick names of the objects to be converted to ROOT histos"
+		)
+
+	def prepare_args(self, parser, plotData):
+		super(ConvertToTGraphErrors, self).prepare_args(parser, plotData)
+		if plotData.plotdict['convert_nicks'] is None:
+			plotData.plotdict['convert_nicks'] = plotData.plotdict['nicks']
+
 	def run(self, plotData=None):
 		super(ConvertToTGraphErrors, self).run(plotData)
-		#TODO make this configurable?
-		for nick in plotData.plotdict['nicks']:
+		for nick in plotData.plotdict['convert_nicks']:
 			hist = plotData.plotdict["root_objects"][nick]
 			tgraph = ROOT.TGraphErrors(hist)
 			plotData.plotdict["root_objects"][nick] = tgraph
