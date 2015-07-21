6cb7fd958b21b79a6f0c2b289032ebc395726d0c
Max Fischer <max.fischer@kit.edu>
2015-07-21 09:53:03 +0200

    updated input fiels to recent skim

--- cfg/excalibur/data15.py
+++ cfg/excalibur/data15.py
@@ -4,10 +4,8 @@ import ZJetConfigBase as base
 def config():
 	cfg = base.getConfig('data', 2015, 'mm')
 	cfg["InputFiles"] = base.setInputFiles(
-		#ekppath="/storage/a/mfischer/skims/MF_AnyMu_2015_746/recent/DoubleMu_Run2015B_Jul2015_13TeV/*.root", # most recent skim, might not be processed completely
-		ekppath="/storage/a/mfischer/skims/MF_AnyMu_2015_746/2015-07-15/MIN_MU_COUNT_2-MIN_MU_PT_8.0/DoubleMu_Run2015B_Jul2015_13TeV/*.root", # wrong pileup density (rho)
-		#ekppath="/storage/a/mfischer/skims/MF_AnyMu_2015_746/2015-07-17/DoubleMu_Run2015B_Jul2015_13TeV/*.root", # no yet ready
-		#nafpath="",
+		ekppath="/storage/a/mfischer/skims/MF_AnyMu_2015_746/recent/DoubleMu_Run2015B_Jul2015_13TeV/*.root",
+		nafpath="/pnfs/desy.de/cms/tier2/store/user/mafische/skims/MF_AnyMu_2015_746/2015-07-17/DoubleMu_Run2015B_Jul2015_13TeV/*.root",
 	)
 	cfg = base.expand(cfg, ['nocuts', 'zcuts', 'noalphanoetacuts', 'noalphacuts', 'noetacuts', 'finalcuts'], ['None', 'L1', 'L1L2L3'])
 
--- cfg/excalibur/mc15.py
+++ cfg/excalibur/mc15.py
@@ -4,10 +4,8 @@ import ZJetConfigBase as base
 def config():
 	cfg = base.getConfig('mc', 2015, 'mm')
 	cfg["InputFiles"] = base.setInputFiles(
-		#ekppath="/storage/a/mfischer/skims/MF_AnyMu_2015_746/recent/DYJetsToLL_M_50_madgraph_Asympt50ns_13TeV/*.root", # most recent skim, might not be processed completely
-		ekppath="/storage/a/mfischer/skims/MF_AnyMu_2015_746/2015-07-15/MIN_MU_COUNT_2-MIN_MU_PT_8.0/DYJetsToLL_M_50_madgraph_Asympt50ns_13TeV/*.root", # wrong pileup density (rho)
-		#ekppath="/storage/a/mfischer/skims/MF_AnyMu_2015_746/2015-07-17/DYJetsToLL_M_50_madgraph_Asympt50ns_13TeV/*.root", # no yet ready
-		#nafpath="",
+		ekppath="/storage/a/mfischer/skims/MF_AnyMu_2015_746/recent/DYJetsToLL_M_50_aMCatNLO_Asympt50ns_13TeV/*.root",
+		nafpath="/pnfs/desy.de/cms/tier2/store/user/mafische/skims/MF_AnyMu_2015_746/2015-07-17/DYJetsToLL_M_50_aMCatNLO_Asympt50ns_13TeV/*.root",
 	)
 	cfg = base.expand(cfg, ['nocuts', 'zcuts', 'noalphanoetacuts', 'noalphacuts', 'noetacuts', 'finalcuts'], ['None', 'L1', 'L1L2L3'])
 	
