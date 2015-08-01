370ceccbab19e915ae480ac37ffa3abd1225db8f
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-08-01 12:39:13 +0200

    Added Powheg MC ee cfg

--- cfg/excalibur/mc_ee_powheg.py
+++ cfg/excalibur/mc_ee_powheg.py
@@ -4,8 +4,8 @@ import mc_ee
 def config():
 	cfg = mc_ee.config()
 	cfg["InputFiles"] = configtools.setInputFiles(
-		#ekppath='/storage/a/dhaitz/skims/2015-07-28_ee-mc_Run2012/kappa_DYToEE*.root",
-		#nafpath='/pnfs/desy.de/cms/tier2/store/user/dhaitz/2015-07-28_ee-mc_Run2012/kappa_DYToEE*.root",
+		ekppath="/storage/a/dhaitz/skims/2015-07-28_ee-mc_Run2012/kappa_DYToEE*.root",
+		nafpath="/pnfs/desy.de/cms/tier2/store/user/dhaitz/2015-07-28_ee-mc_Run2012/kappa_DYToEE*.root",
 	)
 	cfg['CrossSection'] = 1966.7
 	cfg['NumberGeneratedEvents'] = 3297045
