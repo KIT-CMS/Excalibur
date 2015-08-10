1c619460328cbb9e3c8c3fbe87e8cabe9c379f68
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-08-10 16:07:23 +0200

    Added 'leptoncuts' pipeline to ee cfgs

--- cfg/excalibur/data_ee.py
+++ cfg/excalibur/data_ee.py
@@ -7,5 +7,5 @@ def config():
 		ekppath="/storage/a/dhaitz/skims/2015-07-28_ee-data_Run2012/kappa_*.root",
 		nafpath='/pnfs/desy.de/cms/tier2/store/user/dhaitz/2015-07-28_DoubleElectron_Run2012/kappa_*.root'
 	)
-	cfg = configtools.expand(cfg, ['nocuts', 'zcuts'], ['L1L2L3Res'])
+	cfg = configtools.expand(cfg, ['nocuts', 'leptoncuts', 'zcuts'], ['L1L2L3Res'])
 	return cfg
