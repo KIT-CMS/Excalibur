74b01d0c997c50bf4640add9da53b38638f9eeb8
Max Fischer <max.fischer@kit.edu>
2015-08-13 14:10:40 +0200

    now supporting all selection criteria

--- Plotting/plot-configs/python-configs/selection.py
+++ Plotting/plot-configs/python-configs/selection.py
@@ -1,64 +1,200 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 import Excalibur.Plotting.harryinterface as harryinterface
+
+# helper functions
+def _get_input_files(args):
+	args_nofiles = []
+	input_files = []
+	input_file_args = False
+	for elem in args:
+		if not input_file_args and elem == "-i":
+			input_file_args = True
+			continue
+		if input_file_args:
+			if not elem.startswith("-"):
+				input_files.append(elem)
+				continue
+			else:
+				input_file_args = False
+		args_nofiles.append(elem)
+	return input_files, args_nofiles
+
+def _expand_to_files(raw_list, file_count, add_number=False):
+	"""
+	Expand a list of individual arguments to match expansion for multiple input files
+	"""
+	out_list = []
+	for item in raw_list:
+		for _ in xrange(file_count):
+			if add_number:
+				out_list.append(item + _)
+			else:
+				out_list.append(item)
+	return out_list
+
+def cuts_2015(args=None, additional_dictionary=None):
+	d = {
+		"algorithms": ["ak4PFJetsCHS",],
+		"corrections": ["L1L2L3"],
+	}
+	if additional_dictionary:
+		d.update(additional_dictionary)
+	# overwrite harryinterface to get plots in parallel
+	all_plots = {}
+	_hi = harryinterface.harry_interface
+	harryinterface.harry_interface = lambda plots, args: all_plots.setdefault(tuple(args), []).extend(plots)
+	# collect plots
+	muon_isolation(args=args, additional_dictionary=d)
+	cut_n_muons(args=args, additional_dictionary=d)
+	cut_muon_kinematics(args=args, additional_dictionary=d)
+	cut_jet_kinematics(args=args, additional_dictionary=d)
+	# create plots
+	for args in all_plots:
+		_hi(all_plots[args], args)
+
 
 def muon_isolation(args=None, additional_dictionary=None):
 	"""Show criteria for muon isolation based selection"""
-	def make_title(param):
-		title = r"Mu"
-		if "inv" in param:
-			title = "Inv " + title
-		for mid in range(10):
-			if str(mid) in param:
-				title += "%d" % mid
-				break
-		for key, name in (("sumchpt","Ch Had $p_T$"),("sumpet","Phot $E_T$"),("sumnhet","Nt Had $p_T$"),("sumpupt","PileUp $p_T$"),("iso","Iso")):
-			if key in param:
-				title += " %s" % name
-				break
-		if "/mu" in param:
-			title += r" / Mu%d $p_T$" % mid
-		return title
+	arg_plots = {}
+	input_files, args_nofiles = _get_input_files(args)
+	# variables == mu1pt, muinv2sumnhet, ...
+	# ISO == sumchpt + sumnhet + sumpet - 0.5 * sumpupt
+	for muid in (1, 2):
+		for validity in ("inv",""):
+			muon_id = "mu%s%d"%(validity, muid)
+			for obs in ("sumchpt", "sumnhet", "sumpet", "sumpupt"):
+				# 1D plots
+				d = {
+					'x_expressions': ['%s%s/%spt' % (muon_id, obs, muon_id)],
+					'analysis_modules': ['NormalizeToFirstHisto', 'Ratio'],
+					"filename": "cuts_iso_%s_%s" % (muon_id, obs),
+					'x_lims':[0,2],
+					'x_bins': "50,0,2",
+					'weights':['%spt>0'%muon_id],
+				}
+				if additional_dictionary is not None:
+					d.update(additional_dictionary)
+				arg_plots.setdefault(tuple(args), []).append(d)
+				# 2D plots
+				for in_file in input_files:
+					d_args = args_nofiles[:]
+					d_args.extend(("-i", in_file))
+					d = {
+						'x_expressions': ['%siso/%spt' % (muon_id, muon_id)],
+						'y_expressions': ['%s%s/%spt' % (muon_id, obs, muon_id)],
+						'x_bins': "50,0,5",
+						'y_bins': "50,0,5",
+						'z_log': True,
+						"filename": "cuts_iso_%s_%s_%s" % (muon_id, obs, os.path.basename(in_file).split(".",1)[0]),
+						'weights':['%spt>0'%muon_id],
+					}
+					if additional_dictionary is not None:
+						d.update(additional_dictionary)
+					arg_plots.setdefault(tuple(d_args), []).append(d)
+	for d_args in arg_plots:
+		harryinterface.harry_interface(arg_plots[d_args], d_args)
+
+def cut_n_muons(args=None, additional_dictionary=None):
+	"""Plot min/max N muon cut"""
 	plots = []
-	for x_param in [
-				"mu%s%d%s/mu%s%dpt"%(mu_type, mu_idx, param, mu_type, mu_idx)
-					for mu_idx in range(1,3)
-					for param in ("iso","sumchpt","sumnhet","sumpet","sumpupt")
-					for mu_type in ("","inv")
-					]:
-		d = {
-			'x_expressions': [x_param],
-			'x_bins':["20,0,0.5"],
-			#'y_log' : True,
-			'filename' : "muiso_%s"%x_param.replace("/","_"),
-			'analysis_modules': ['NormalizeToFirstHisto', 'Ratio'],
-			"algorithms": ["ak4PFJetsCHS",],
-			"corrections": ["L1L2L3"],
-			'title': make_title(x_param),
-			'y_subplot_lims': [0, 2],
-			}
-		if additional_dictionary is not None:
-			d.update(additional_dictionary)
-		plots.append(d)
-	for x_param in [
-				"mu%s%d%s"%(mu_type, mu_idx, param)
-					for mu_idx in range(1,3)
-					for param in ("iso","sumchpt","sumnhet","sumpet","sumpupt")
-					for mu_type in ("","inv")
-					]:
-		d = {
-			'x_expressions': [x_param],
-			'x_bins':["20,0,20"],
-			#'y_log' : True,
-			'filename' : "muiso_%s"%x_param.replace("/","_"),
-			'analysis_modules': ['NormalizeToFirstHisto', 'Ratio'],
-			"algorithms": ["ak4PFJetsCHS",],
-			"corrections": ["L1L2L3"],
-			'title': make_title(x_param),
-			'y_subplot_lims': [0, 2],
-			}
-		if additional_dictionary is not None:
-			d.update(additional_dictionary)
-		plots.append(d)
+	d_base = {
+		'x_bins': ["6,-0.5,5.5"],
+		'plot_modules': ["PlotMplZJet", "PlotMplRectangle"],
+		"rectangle_x": [-1,1.5,3.5,6],
+		"rectangle_alpha": [0.2],
+		"rectangle_color": ["red","red"],
+		"y_log": True,
+		'y_subplot_lims': [0.15, 1.95],
+	}
+	d1 = {
+		"x_expressions": ["nmuons", "nmuons"],
+		"markers": ["o", "fill"]*1 + ["o"]*1,
+		"nicks": ["data", "mc"],
+		"labels": [r"$\\mu_\\mathrm{Data}$", r"$\\mu_\\mathrm{MC}$"],
+		'analysis_modules': ['Ratio'],
+		"filename": "cuts_nmuons",
+		"title": "Number of valid Muons",
+	}
+	d2 = {
+		"x_expressions": ["nmuonsinv+nmuons", "nmuonsinv+nmuons"],
+		"markers": ["o", "fill"]*2 + ["o"]*2,
+		"nicks": ["data Mu Val", "mc Mu Val", "data Mu Tot", "mc Mu Tot"],
+		"labels": [r"$\\mu_\\mathrm{Data}$", r"$\\mu_\\mathrm{MC}$", r"$\\mu_\\mathrm{Data}^\\mathrm{tot}$", r"$\\mu_\\mathrm{MC}^\\mathrm{tot}$"],
+		"filename": "cuts_totmuons",
+		"title": "Number of total Muons",
+	}
+	d1.update(d_base)
+	d2.update(d_base)
+	if additional_dictionary is not None:
+		d1.update(additional_dictionary)
+		d2.update(additional_dictionary)
+	plots.append(d1)
+	plots.append(d2)
 	harryinterface.harry_interface(plots, args)
+
+def cut_muon_kinematics(args=None, additional_dictionary=None):
+	"""Plot pt and eta muon cut"""
+	arg_plots = {}
+	input_files, args_nofiles = _get_input_files(args)
+	for in_file in input_files:
+		args = args_nofiles[:]
+		args.extend(("-i", in_file))
+		for muid in range(1,3):
+			d = {
+				'x_expressions': ['mu%dpt' % muid],
+				'y_expressions': ['mu%deta' % muid],
+				'x_bins': "25,0,100",
+				'y_bins': "25,0,2.5",
+				'y_lims': [0,2.5],
+				'x_lims': [0,100],
+				#'z_log': True,
+				'plot_modules': ["PlotMplZJet", "PlotMplRectangle"],
+				# cfg['CutMuonPtMin'] = 20.0
+				# cfg['CutMuonEtaMax'] = 2.3
+				"rectangle_x": [-1,20],
+				"rectangle_y": [0,2.3, 2.3,5],
+				"rectangle_alpha": [0.2],
+				"rectangle_color": ["red","red"],
+				"filename": "cuts_muon%d_%s"%(muid,os.path.basename(in_file).split(".",1)[0]),
+				"title": "Muon %d"%muid,
+			}
+			if additional_dictionary is not None:
+				d.update(additional_dictionary)
+			arg_plots.setdefault(tuple(args), []).append(d)
+	for args in arg_plots:
+		harryinterface.harry_interface(arg_plots[args], args)
+
+def cut_jet_kinematics(args=None, additional_dictionary=None):
+	"""Plot pt and eta jet1 cut"""
+	arg_plots = {}
+	input_files, args_nofiles = _get_input_files(args)
+	for in_file in input_files:
+		args = args_nofiles[:]
+		args.extend(("-i", in_file))
+		for muid in range(1,2):
+			d = {
+				'x_expressions': ['jet%dpt' % muid],
+				'y_expressions': ['jet%deta' % muid],
+				'x_bins': "25,0,100",
+				'y_bins': "25,0,2.5",
+				'y_lims': [0,2.5],
+				'x_lims': [0,100],
+				#'z_log': True,
+				'plot_modules': ["PlotMplZJet", "PlotMplRectangle"],
+				# cfg['CutLeadingJetPtMin'] = 12.0
+				# cfg['CutLeadingJetEtaMax'] = 1.3
+				"rectangle_x": [-1,12],
+				"rectangle_y": [0,1.3, 1.3,5],
+				"rectangle_color": ["red","red"],
+				"rectangle_alpha": [0.2],
+				"filename": "cuts_jet%d_%s" % (muid, os.path.basename(in_file).split(".",1)[0]),
+				"title": "Jet %d"%muid,
+			}
+			if additional_dictionary is not None:
+				d.update(additional_dictionary)
+			arg_plots.setdefault(tuple(args), []).append(d)
+	for args in arg_plots:
+		harryinterface.harry_interface(arg_plots[args], args)
