5a76dae17d07d5a962a27f60a46aea2771b4de1d
Max Fischer <max.fischer@kit.edu>
2015-09-02 09:12:36 +0200

    added 25ns configs

---
+++ cfg/excalibur/data15_25ns.py
@@ -0,0 +1,16 @@
+import configtools
+
+
+def config():
+	cfg = configtools.getConfig('data', 2015, 'mm')
+	cfg["InputFiles"] = configtools.setInputFiles(
+		ekppath="/storage/a/mfischer/skims/zjet/2015_08_31/DoubleMu_Run2015C_Aug2015_13TeV/*.root",
+		nafpath="/pnfs/desy.de/cms/tier2/store/user/mafische/skims/MF_AnyMu_2015_746/2015-08-31/DoubleMu_Run2015C_Aug2015_13TeV/*.root",
+	)
+	cfg = configtools.expand(cfg, ['nocuts', 'zcuts', 'noalphanoetacuts', 'noalphacuts', 'noetacuts', 'finalcuts'], ['None', 'L1', 'L1L2L3', 'L1L2L3Res'])
+	cfg['JsonFiles'] = [configtools.getPath() + '/data/json/Cert_246908-254879_13TeV_PromptReco_Collisions15_JSON.txt']
+	configtools.remove_quantities(cfg, ['jet1btag','jet1qgtag', 'jet1ptl1l2l3', 'jet1res', 'jet1rc'])
+
+	cfg['CutAlphaMax'] = 0.3
+
+	return cfg
---
+++ cfg/excalibur/mc15_25ns.py
@@ -0,0 +1,24 @@
+import configtools
+
+
+def config():
+	cfg = configtools.getConfig('mc', 2015, 'mm')
+	cfg["InputFiles"] = configtools.setInputFiles(
+		ekppath="/storage/a/mfischer/skims/zjet/2015_08_31/DYJetsToLL_M_50_aMCatNLO_Asympt25ns_13TeV/*.root",
+		nafpath="/pnfs/desy.de/cms/tier2/store/user/mafische/skims/MF_AnyMu_2015_746/2015-08-31/DYJetsToLL_M_50_aMCatNLO_Asympt25ns_13TeV/*.root",
+	)
+	cfg = configtools.expand(cfg, ['nocuts', 'zcuts', 'noalphanoetacuts', 'noalphacuts', 'noetacuts', 'finalcuts', 'betacuts'], ['None', 'L1', 'L1L2L3'])
+	
+	configtools.remove_quantities(cfg, ['jet1btag','jet1qgtag', 'jet1rc'])
+
+	cfg['JetMatchingAlgorithm'] = 'algorithmic'
+	#cfg['CutAlphaMax'] = 0.3
+	cfg['DeltaRRadiationJet'] = 1
+
+	cfg['CutAlphaMax'] = 0.3
+	cfg['CutBetaMax'] = 0.1
+
+	cfg['NumberGeneratedEvents'] = 299269
+	cfg['CrossSection'] = 6025.2  # https://twiki.cern.ch/twiki/bin/viewauth/CMS/StandardModelCrossSectionsat13TeV
+
+	return cfg
