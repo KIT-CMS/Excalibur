45986efd6fc1442b567277ed63a1f087061a3b4d
Max Fischer <max.fischer@kit.edu>
2015-09-02 09:17:02 +0200

    updated 25ns lumi

--- cfg/excalibur/data15_25ns.py
+++ cfg/excalibur/data15_25ns.py
@@ -9,6 +9,7 @@ def config():
 	)
 	cfg = configtools.expand(cfg, ['nocuts', 'zcuts', 'noalphanoetacuts', 'noalphacuts', 'noetacuts', 'finalcuts'], ['None', 'L1', 'L1L2L3', 'L1L2L3Res'])
 	cfg['JsonFiles'] = [configtools.getPath() + '/data/json/Cert_246908-254879_13TeV_PromptReco_Collisions15_JSON.txt']
+	cfg['Lumi'] = 0.0521
 	configtools.remove_quantities(cfg, ['jet1btag','jet1qgtag', 'jet1ptl1l2l3', 'jet1res', 'jet1rc'])
 
 	cfg['CutAlphaMax'] = 0.3
