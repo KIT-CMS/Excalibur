f6d8a36f8da8a4dd7db59939b019cd64761ef75d
Joram Berger <joram.berger@cern.ch>
2015-09-03 18:06:55 +0200

    Checkout only latest commits for testing

--- .install-dependencies.sh
+++ .install-dependencies.sh
@@ -11,11 +11,11 @@ echo "/usr/local/lib/root" | sudo tee -a /etc/ld.so.conf
 sudo ldconfig || exit 1
 
 # Install Kappa
-git clone https://github.com/KappaAnalysis/Kappa.git || exit 1
+git clone --depth=10 https://github.com/KappaAnalysis/Kappa.git || exit 1
 make -C Kappa/DataFormats/test || exit 1
 
 # Install KappaTools
-git clone https://github.com/KappaAnalysis/KappaTools.git || exit 1
+git clone --depth=10 https://github.com/KappaAnalysis/KappaTools.git || exit 1
 make -C KappaTools || exit 1
 
 # Install CondFormats
@@ -23,7 +23,7 @@ wget http://www-ekp.physik.uni-karlsruhe.de/~sieber/CondFormats.tar.gz || exit 1
 tar xzf CondFormats.tar.gz || exit 1
 
 #Install Artus
-git clone https://github.com/artus-analysis/Artus.git
+git clone --depth=10 https://github.com/artus-analysis/Artus.git
 cd Artus
 cmake .
 make
