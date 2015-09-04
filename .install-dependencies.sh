417d7a4356753c3b70585002f8a4daf6c52e1e66
Joram Berger <joram.berger@cern.ch>
2015-09-04 12:04:15 +0200

    Request Ubuntu 14.04 for testing
    By using service: docker, a VM with Ubuntu 14.04 is requested.
    Boost, root and gcc can be directly installed from there
    including pyroot which was missing before.

--- .install-dependencies.sh
+++ .install-dependencies.sh
@@ -3,13 +3,6 @@
 BASE=$(cd .. && echo $PWD)
 cd $BASE
 
-# Install Root
-wget http://www-ekp.physik.uni-karlsruhe.de/~sieber/root_5.34.07-1_amd64.deb || exit 1
-sudo dpkg -i root_5.34.07-1_amd64.deb || exit 1
-echo "/usr/local/lib" | sudo tee -a /etc/ld.so.conf
-echo "/usr/local/lib/root" | sudo tee -a /etc/ld.so.conf
-sudo ldconfig || exit 1
-
 # Install Kappa
 git clone --depth=10 https://github.com/KappaAnalysis/Kappa.git || exit 1
 make -C Kappa/DataFormats/test || exit 1
