b169c29eb8a79666a65e58b87c1157579fa4ded6
Max Fischer <max.fischer@kit.edu>
2015-09-24 17:14:56 +0200

    ZProducer: removed max lepton requirement, now enforcing a single unambigious Z per event

--- src/Producers/ZProducer.h
+++ src/Producers/ZProducer.h
@@ -5,7 +5,7 @@
 /** Producer class for Z boson reconstruction from muons/electrons.
  *
  *	Needs to run after the valid object producers.
- * 	Needs min 2 valid leptons and max 3 valid leptons!
+ * 	Needs at least 2 valid leptons to construct a Z.
  */
 
 template <class TLepton1, class TLepton2>
@@ -23,42 +23,48 @@ class ZProducerBase : public ZJetProducerBase
                          ZJetSettings const& settings) const override
 
     {
-        // Create all possible Z combinations
-        // Note: If we have more than 3 muons in an event, this may produce double
-        // counting
-        std::vector<KLV> z_cand;
-        std::vector<std::pair<KLepton*, KLepton*>> z_leptons;
+        /*
+        Try all lepton combinations to find a valid Z
+
+        Exactly one Z must be reconstructed. Events with any more or less are
+        deemed ambigious, not having *a* valid Z.
+        */
+        int found_zs = 0;
+        KLV z_cand;
+        std::pair<KLepton*, KLepton*> z_leptons;
+        // do not double count when matching leptons from the same collection
+        // NOTE: this check could probably be done more elegantly...
+        bool same_ll_collection = ((static_cast<void*>(&(product.*m_validLeptonsMember1))) ==
+                                   (static_cast<void*>(&(product.*m_validLeptonsMember2))));
         for (unsigned int i = 0; i < (product.*m_validLeptonsMember1).size(); ++i) {
-            for (unsigned int j = i + 1; j < (product.*m_validLeptonsMember2).size(); ++j) {
+            for (unsigned int j = (same_ll_collection ? i + 1 : 0);
+                 j < (product.*m_validLeptonsMember2).size();
+                 ++j) {
                 KLepton* const m1 = (product.*m_validLeptonsMember1).at(i);
                 KLepton* const m2 = (product.*m_validLeptonsMember2).at(j);
+                // valid Z is neutral and close to Z mass
                 if (m1->charge() + m2->charge() == 0) {
                     KLV z;
                     z.p4 = m1->p4 + m2->p4;
                     if (z.p4.mass() > settings.GetZMass() - settings.GetZMassRange() &&
                         z.p4.mass() < settings.GetZMass() + settings.GetZMassRange()) {
-                        z_cand.emplace_back(z);
+                        // allow only 1 Z per event
+                        if (++found_zs > 1)
+                            break;
+                        z_cand = z;
                         if (m1->p4.Pt() > m2->p4.Pt())
-                            z_leptons.emplace_back(m1, m2);
+                            z_leptons = std::make_pair(m1, m2);
                         else
-                            z_leptons.emplace_back(m2, m1);
+                            z_leptons = std::make_pair(m2, m1);
                     }
                 }
             }
+            if (found_zs > 1)
+                break;
         }
-        // If there is more than one valid Z candidate we chose the one with its
-        // mass closest to the Z mass.
-        if (z_cand.size() > 0) {
-            unsigned int best_z_cand = 9999;
-            for (unsigned int i = 0; i < z_cand.size(); ++i) {
-                if (best_z_cand == 9999 ||
-                    fabs(z_cand[i].p4.mass() - settings.GetZMass()) <=
-                        fabs(z_cand[best_z_cand].p4.mass() - settings.GetZMass())) {
-                    best_z_cand = i;
-                }
-            }
-            product.m_z = z_cand[best_z_cand];
-            product.m_zLeptons = z_leptons.at(best_z_cand);
+        if (found_zs == 1) {
+            product.m_z = z_cand;
+            product.m_zLeptons = z_leptons;
             product.m_validZ = true;
         } else {
             product.m_validZ = false;
