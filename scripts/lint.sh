e014cf0af646b3f7b58a8cafca19211c1b6d2988
Joram Berger <joram.berger@cern.ch>
2015-09-25 14:15:04 +0200

    Add cppcheck to lint script
    This is a general C++ code linter. It can help us to improve our code if used.
    As it checks all dependencies carefully, we must ignore badly readable code
    like the easylogging++ which is copied into Artus.

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -6,7 +6,7 @@ if [[ "$@" == "-h" || -z "$@" ]]; then
 fi
 GREPOPT="-I --include *.h --include *.cc --include *.cpp --include *.hxx --exclude=easylogging++.h $@"
 
-# use the Google linter without these filters:
+# use the Google linter cpplint without these filters:
 # - header_guard: we use pragma once
 # - include_what_you_use: we have global includes for map, string, vector
 # - legal: we have no copyright notice
@@ -16,6 +16,73 @@ if [[ "$@" == "google" ]]; then
     find src -name "*cc" -or -name "*h" | xargs python own/cpplint.py --linelength=100 \
     --filter=-build/header_guard,-build/include_what_you_use,-legal,-readability/braces,-runtime/int,-whitespace/braces,-whitespace/indent,-whitespace/newline
     exit 0
+fi
+
+# use cppcheck if installed
+# all _ELPP* and __* lines are necessary because of the ugly macros of easylogging++
+# does not check ROOT or system headers, will complain about missing headers in the end
+if [[ "$1" == "cpp" ]]; then
+  which cppcheck &> /dev/null || (echo "cppcheck not installed" && exit 1)
+  PLUSARTUS=""
+  if [[ "$2" == "all" ]]; then
+    PLUSARTUS="-I .."
+  fi
+cppcheck --platform=unix64 --std=c++11 --language=c++ --enable=all \
+-I src $PLUSARTUS -I ../CondFormats \
+--suppress=missingIncludeSystem \
+--suppress=unusedFunction \
+--suppress=uninitMemberVar \
+--suppress=*:../Artus/Utility/interface/easylogging++.h \
+--template="+{line} {file} ({severity}) {id}: {message}" \
+-U_ELPP_AS_DLL \
+-U_ELPP_BOOST_LOGGING \
+-U_ELPP_COMPILER_MSVC \
+-U_ELPP_DEFAULT_LOG_FILE \
+-U_ELPP_DISABLE_ASSERT \
+-U_ELPP_DISABLE_CONFIGURATION_FROM_PROGRAM_ARGS \
+-U_ELPP_DISABLE_CUSTOM_FORMAT_SPECIFIERS \
+-U_ELPP_DISABLE_DEFAULT_CRASH_HANDLING \
+-U_ELPP_DISABLE_LOGGING_FLAGS_FROM_ARG \
+-U_ELPP_DISABLE_LOGS \
+-U_ELPP_DISABLE_LOG_FILE_FROM_ARG \
+-U_ELPP_DISABLE_PERFORMANCE_TRACKING \
+-U_ELPP_DISABLE_VMODULES \
+-U_ELPP_DISABLE_VMODULES_EXTENSION \
+-U_ELPP_ENABLE_ERRORS \
+-U_ELPP_ENABLE_INFO \
+-U_ELPP_HANDLE_POST_LOG_DISPATCH \
+-U_ELPP_HANDLE_POST_PERFORMANCE_TRACKING \
+-U_ELPP_HANDLE_SIGABRT \
+-U_ELPP_LOG_STD_ARRAY \
+-U_ELPP_LOG_UNORDERED_MAP \
+-U_ELPP_STL_LOGGING \
+-U_ELPP_MULTI_LOGGER_SUPPORT \
+-U_ELPP_PERFORMANCE_DISABLE_COMPARE_CHECKPOINTS \
+-U_ELPP_PERFORMANCE_MICROSECONDS \
+-U_ELPP_QT_LOGGING \
+-U_ELPP_STOP_ON_FIRST_ASSERTION \
+-U_ELPP_STRICT_SIZE_CHECK \
+-U_ELPP_SYSLOG \
+-U_ELPP_THREAD_SAFE \
+-U_ELPP_UNICODE \
+-U_ELPP_WXWIDGETS_LOGGING \
+-U_LOGGER \
+-U_MSC_VER \
+-U_MSC_VER=1600 \
+-U_PERFORMANCE_LOGGER \
+-U__ANDROID__ \
+-U__APPLE__ \
+-U__CYGWIN__ \
+-U__FreeBSD__ \
+-U__GNUC__ \
+-U__GXX_EXPERIMENTAL_CXX0X__ \
+-U__INTEL_COMPILER \
+-U__clang__ \
+-UKAPPA_STANDALONE_DICT \
+-DM_PI=3.1415 \
+--force \
+$EXCALIBURPATH/src
+exit 0
 fi
 
 # checks with a regex pattern and an advice
