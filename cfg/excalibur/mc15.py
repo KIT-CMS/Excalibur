64e7223b1cdd3e17846168e10de7b092ee387b94
Max Fischer <max.fischer@kit.edu>
2015-10-07 23:18:19 +0200

    updated skim

--- cfg/excalibur/mc15.py
+++ cfg/excalibur/mc15.py
@@ -4,7 +4,7 @@ import configtools
 def config():
 	cfg = configtools.getConfig('mc', 2015, 'mm')
 	cfg["InputFiles"] = configtools.setInputFiles(
-		ekppath="/storage/a/mfischer/skims/MF_AnyMu_2015_746/2015-07-24//DYJetsToLL_M_50_aMCatNLO_Asympt50ns_13TeV/*.root",
+		ekppath="/storage/a/mfischer/skims/zjet/2015_09_07/DYJetsToLL_M_50_aMCatNLO_Asympt50ns_13TeV/*.root",
 		nafpath="/pnfs/desy.de/cms/tier2/store/user/mafische/skims/MF_AnyMu_2015_746/2015-07-24//DYJetsToLL_M_50_aMCatNLO_Asympt50ns_13TeV/*.root",
 	)
 	cfg = configtools.expand(cfg, ['nocuts', 'zcuts', 'noalphanoetacuts', 'noalphacuts', 'noetacuts', 'finalcuts', 'betacuts'], ['None', 'L1', 'L1L2L3'])
