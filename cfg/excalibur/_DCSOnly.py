fd26ada5929de142ef57ae3ea662508c4e53e9c2
Max Fischer <max.fischer@kit.edu>
2015-10-16 15:59:53 +0200

    added config mod for DCSOnly

---
+++ cfg/excalibur/_DCSOnly.py
@@ -0,0 +1,10 @@
+import configtools
+
+
+def modify_config(cfg):
+	"""
+	Use runs from DCSONLY Json
+	"""
+	cfg['Jec'] = ["/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/DCSOnly/json_DCSONLY.txt"]
+	configtools.get_lumi(json_source=cfg['JsonFiles'][0])
+	return cfg
