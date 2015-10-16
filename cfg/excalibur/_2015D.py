2678316b29b4360c7fecd89d8327d0e13d7ddd0e
Max Fischer <max.fischer@kit.edu>
2015-10-16 15:59:33 +0200

    2015C/D calculate lumi with correct ranges

--- cfg/excalibur/_2015D.py
+++ cfg/excalibur/_2015D.py
@@ -1,5 +1,6 @@
 import configtools
 
+
 def modify_config(cfg):
 	"""
 	Use 2015D data only
@@ -8,4 +9,5 @@ def modify_config(cfg):
 		ekppath="/storage/a/mfischer/skims/zjet/2015-10-09/DoubleMu_Run2015D_Sep2015_13TeV/*.root",
 		nafpath="/pnfs/desy.de/cms/tier2/store/user/mafische/skims/MF_AnyMu_2015_746/2015-10-09/DoubleMu_Run2015D_Sep2015_13TeV/*.root",
 	)
+	configtools.get_lumi(json_source=cfg['JsonFiles'][0], min_run=256629., max_run=float("inf"))
 	return cfg
