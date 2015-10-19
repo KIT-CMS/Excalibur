7746db8291f3ccd2d46e3d5f9158cee33b0671a9
Julia Handl <uxdbn@student.kit.edu>
2015-10-19 13:45:57 +0200

    Added finalcuts for mc_ee

--- cfg/excalibur/mc_ee.py
+++ cfg/excalibur/mc_ee.py
@@ -7,9 +7,8 @@ def config():
 		ekppath='/storage/a/dhaitz/skims/2015-07-28_ee-mc_Run2012/kappa_DYJ*.root',
 		nafpath='/pnfs/desy.de/cms/tier2/store/user/dhaitz/2015-07-28_ee-mc_Run2012/kappa_DYJ*.root'
 	)
-	cfg = configtools.expand(cfg, ['nocuts', 'leptoncuts', 'zcuts'], ['L1L2L3'])
+	cfg = configtools.expand(cfg, ['nocuts', 'leptoncuts', 'zcuts', 'finalcuts'], ['L1L2L3'])
 
 	cfg['NumberGeneratedEvents'] = 30459503
 	cfg['CrossSection'] = 3503.71
-
 	return cfg
