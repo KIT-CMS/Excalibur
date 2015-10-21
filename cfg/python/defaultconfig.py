c9131f7dc36f750dc522b4954081361ec75564b1
Max Fischer <max.fischer@kit.edu>
2015-10-21 18:48:29 +0200

    now using correct pileup file for 13TEV

--- cfg/python/defaultconfig.py
+++ cfg/python/defaultconfig.py
@@ -403,7 +403,7 @@ def data_2012(cfg, **kwargs):
 def data_2015(cfg, **kwargs):
 	cfg['Processors'] += ['producer:NPUProducer']
 	cfg['Minbxsec'] = 80.0
-	cfg['NPUFile'] = configtools.getPath() + '/data/pileup/pumean_data.txt'
+	cfg['NPUFile'] = configtools.getPath() + '/data/pileup/pumean_data_13TEV.txt'
 	cfg['Pipelines']['default']['Quantities'] += ['npumean']
 	# JSON & JEC for 50ns and 25ns - see /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV
 	if kwargs.get('bunchcrossing', "50ns") == "50ns":
