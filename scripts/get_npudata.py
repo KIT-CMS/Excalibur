063c1e71058fd27d0be85eec81b77537d70dd25c
Max Fischer <max.fischer@kit.edu>
2015-10-21 17:56:04 +0200

    added script for creating NPUProducer input file and sample

---
+++ scripts/get_npudata.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+# standard library imports
+import json
+import argparse
+
+# third party imports
+
+# application/library imports
+
+CLI = argparse.ArgumentParser(
+	description="Reformats pileup JSON for the Excalibur NPUProducer",
+	epilog="""
+	Output format is a space separated csv with the columns:
+	run lumi_section lumi cross_section_rms cross_section_avg
+	"""
+)
+CLI.add_argument(
+	"-j",
+	"--pileup_json",
+	default="/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/PileUp/pileup_latest.txt",
+	help="JSON file containing pileup information",
+)
+CLI.add_argument(
+	"-o",
+	"--out-path",
+	default="npuproducer_input.txt",
+)
+
+def converter(pileup_json):
+	print "Reading JSON... ",
+	with open(pileup_json) as pileup_source:
+		raw_data = json.load(pileup_source)
+	print "done"
+	for run in sorted(raw_data):
+		for lumi_section, lumi, xs_rms, xs_avg in sorted(raw_data[run]):
+			yield "%d %d %f %f %f" % (int(run), int(lumi_section), lumi, xs_rms, xs_avg)
+
+if __name__ == "__main__":
+	opts = CLI.parse_args()
+	print "Reading JSON..."
+	with open(opts.pileup_json) as pileup_source:
+		raw_data = json.load(pileup_source)
+	with open(opts.out_path, "w") as out_file:
+		for run_idx, run in enumerate(sorted(raw_data)):
+			print "Writing: run %3d/%3d\r" % (run_idx+1, len(raw_data)),
+			for lumi_section, lumi, xs_rms, xs_avg in sorted(raw_data[run]):
+				out_file.write("%d %d %g %g %g\n" % (int(run), lumi_section, lumi, xs_rms, xs_avg))
+	print
