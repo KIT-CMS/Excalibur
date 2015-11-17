6ca0b4b21ac822b7686e7917f187663f29183a6e
Max Fischer <max.fischer@kit.edu>
2015-11-17 16:01:49 +0100

    switched to current 25ns corrections

--- cfg/python/defaultconfig.py
+++ cfg/python/defaultconfig.py
@@ -414,7 +414,7 @@ def data_2015(cfg, **kwargs):
 		cfg['Jec'] = configtools.getPath() + '/data/jec/Summer15_50nsV5_DATA/Summer15_50nsV5_DATA'
 		cfg['Lumi'] = 0.04003
 	elif kwargs.get('bunchcrossing', "50ns") == "25ns":
-		cfg['Jec'] = configtools.get_jec("Summer15_25nsV5_DATA")
+		cfg['Jec'] = configtools.get_jec("Summer15_25nsV6_DATA")
 		cfg['Lumi'] = configtools.get_lumi(json_source=cfg['JsonFiles'])
 	else:
 		raise ValueError("No support for 'bunchcrossing' %r" % kwargs['bunchcrossing'])
@@ -445,7 +445,7 @@ def mc_2015(cfg, **kwargs):
 	if kwargs.get('bunchcrossing', "50ns") == "50ns":
 		cfg['Jec'] = configtools.getPath() + '/data/jec/Summer15_50nsV5_MC/Summer15_50nsV5_MC'
 	elif kwargs['bunchcrossing'] == "25ns":
-		cfg['Jec'] = configtools.get_jec("Summer15_25nsV5_MC")
+		cfg['Jec'] = configtools.get_jec("Summer15_25nsV6_MC")
 	else:
 		raise ValueError("No support for 'bunchcrossing' %r" % kwargs['bunchcrossing'])
 
