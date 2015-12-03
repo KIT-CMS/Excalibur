5f7d677023fa7c8c1e8309c4dc5ee4e1039b08e7
Joram Berger <joram.berger@cern.ch>
2015-12-03 19:26:44 +0100

    Avoid deep nesting in ValidZllJetsProducer

--- src/Producers/ValidZllJetsProducer.cc
+++ src/Producers/ValidZllJetsProducer.cc
@@ -31,32 +31,33 @@ void ValidZllJetsProducer::Produce(ZJetEvent const& event,
     only one jet vetoed by every lepton.
     */
     // nothing to do if there are no Zlls
-    if (product.m_zValid) {
-        std::size_t zl1_idx = -1u;
-        std::size_t zl2_idx = -1u;
-        for (std::size_t i = 0; i < product.m_validJets.size(); ++i) {
-            if (ROOT::Math::VectorUtil::DeltaR(product.m_validJets.at(i)->p4,
-                                               product.m_zLeptons.first->p4) <
-                minZllJetDeltaRVeto) {
-                zl1_idx = i;
-                if (zl2_idx < product.m_validJets.size()) {
-                    break;
-                }
-            } else if (ROOT::Math::VectorUtil::DeltaR(product.m_validJets.at(i)->p4,
-                                                      product.m_zLeptons.second->p4) <
-                       minZllJetDeltaRVeto) {
-                zl2_idx = i;
-                if (zl1_idx < product.m_validJets.size()) {
-                    break;
-                }
+    if (!product.m_zValid)
+        return;
+
+    std::size_t zl1_idx = -1u;
+    std::size_t zl2_idx = -1u;
+    for (std::size_t i = 0; i < product.m_validJets.size(); ++i) {
+        if (ROOT::Math::VectorUtil::DeltaR(product.m_validJets.at(i)->p4,
+                                           product.m_zLeptons.first->p4) < minZllJetDeltaRVeto) {
+            zl1_idx = i;
+            if (zl2_idx < product.m_validJets.size()) {
+                break;
+            }
+        } else if (ROOT::Math::VectorUtil::DeltaR(product.m_validJets.at(i)->p4,
+                                                  product.m_zLeptons.second->p4) <
+                   minZllJetDeltaRVeto) {
+            zl2_idx = i;
+            if (zl1_idx < product.m_validJets.size()) {
+                break;
             }
         }
-        // zl1 has higher pT, i.e. likely vetoes a higher pT jet that should be erased last
-        if (zl1_idx > zl2_idx)
-            std::swap(zl1_idx, zl2_idx);
-        if (zl2_idx < product.m_validJets.size())
-            product.m_validJets.erase(product.m_validJets.begin() + zl2_idx);
-        if (zl1_idx < product.m_validJets.size())
-            product.m_validJets.erase(product.m_validJets.begin() + zl1_idx);
     }
+
+    // zl1 has higher pT, i.e. likely vetoes a higher pT jet that should be erased last
+    if (zl1_idx > zl2_idx)
+        std::swap(zl1_idx, zl2_idx);
+    if (zl2_idx < product.m_validJets.size())
+        product.m_validJets.erase(product.m_validJets.begin() + zl2_idx);
+    if (zl1_idx < product.m_validJets.size())
+        product.m_validJets.erase(product.m_validJets.begin() + zl1_idx);
 }
