fe1291bae13b9d2f265a4793bc42ac77de6078e9
Max Fischer <max.fischer@kit.edu>
2015-12-08 10:09:04 +0100

    modifier configs for 2015C and D are now compatible with Zee

--- cfg/excalibur/_2015D.py
+++ cfg/excalibur/_2015D.py
@@ -5,9 +5,4 @@ def modify_config(cfg):
 	Use 2015D data only
 	"""
 	cfg['JsonFiles'].add_run_range(256629, float("inf"))
-	if cfg['InputIsData']:
-		cfg["InputFiles"].set_input(
-			ekppath="/storage/a/mfischer/skims/zjet/2015-10-30/Zmm_DoubleMu_Run2015D_*_13TeV/*.root",
-			nafpath="/pnfs/desy.de/cms/tier2/store/user/mafische/skims/MF_Zll_run2/2015-10-30/Zmm_DoubleMu_Run2015D*_13TeV/*.root"
-		)
 	return cfg
