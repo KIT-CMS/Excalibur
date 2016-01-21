1bea10358ce3e4bd7fbf7fbf90e2798f03f26d60
Max Fischer <max.fischer@kit.edu>
2016-01-21 14:16:17 +0100

    excalibur: '--set-opts' now works for InputFiles and OutputPath

--- scripts/excalibur.py
+++ scripts/excalibur.py
@@ -76,15 +76,15 @@ def ZJet():
 	# always make json config unless we resume existing one
 	if not options.isjson and not options.resume:
 		conf = import_config(options.cfg, options.config_mods)
-		conf["InputFiles"] = createFileList(conf["InputFiles"], options.fast)
-		if conf["OutputPath"] == "out":
-			conf["OutputPath"] = options.out + '.root'
 		if options.skip:
 			conf['FirstEvent'] = options.skip
 		if options.nevents:
 			conf['ProcessNEvents'] = options.nevents
 		for key, value in options.set_opts:
 			conf[key] = value
+		conf["InputFiles"] = createFileList(conf["InputFiles"], options.fast)
+		if conf["OutputPath"] == "out":
+			conf["OutputPath"] = options.out + '.root'
 		writeJson(conf, options.json)
 		wrapper_logger.info("%d pipelines configured, written to %s", len(conf["Pipelines"]), options.json)
 	# get an existing one
