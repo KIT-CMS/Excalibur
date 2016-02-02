5349e903c81fa7c6f387aa8ea95ab1e942db7706
Max Fischer <max.fischer@kit.edu>
2016-02-02 09:15:33 +0100

    cfg: updated JEC

--- cfg/excalibur/data15_25ns_ee.py
+++ cfg/excalibur/data15_25ns_ee.py
@@ -7,6 +7,6 @@ def config():
 		ekppath="/storage/a/mfischer/skims/zjet/2016-01-19/Zee_Zee_Run2015D-16Dec2015-v2/*.root",
 		nafpath="/pnfs/desy.de/cms/tier2/store/user/mafische/skims/MF_Zll_run2/2016-01-19/Zee_Zee_Run2015D-16Dec2015-v2/*.root"
 	)
-	cfg = configtools.expand(cfg, ['nocuts', 'zcuts', 'noalphanoetacuts', 'noalphacuts', 'noetacuts', 'finalcuts'], ['None', 'L1', 'L1L2L3', 'L1L2L3Res'])
+	cfg = configtools.expand(cfg, ['nocuts', 'zcuts', 'noalphanoetacuts', 'noalphacuts', 'noetacuts', 'finalcuts'], ['None', 'L1', 'L1L2L3'])#,, 'L1L2L3Res'])
 	configtools.remove_quantities(cfg, ['jet1btag', 'jet1qgtag', 'jet1ptl1l2l3', 'jet1res', 'jet1rc', "e1mvanontrig", "e1mvatrig", "e2mvanontrig", "e2mvatrig"])
 	return cfg
