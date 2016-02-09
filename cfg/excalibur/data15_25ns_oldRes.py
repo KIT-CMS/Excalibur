88fc56410b092019dfe87e12113913a79e025d4b
cheidecker <christoph.heidecker@student.kit.edu>
2016-02-09 09:32:44 +0100

    Created configs to run 7.6 dataset with summer15 residual corrections

---
+++ cfg/excalibur/data15_25ns_oldRes.py
@@ -0,0 +1,12 @@
+import configtools
+
+
+def config():
+	cfg = configtools.getConfig('data', 2015, 'mm', bunchcrossing='25ns')
+	cfg["InputFiles"].set_input(
+		ekppath="/storage/a/mfischer/skims/zjet/2016-01-19/Zmm_Zmm_Run2015D-16Dec2015-v1/*.root",
+		nafpath="/pnfs/desy.de/cms/tier2/store/user/mafische/skims/MF_Zll_run2/2016-01-19/Zmm_Zmm_Run2015D-16Dec2015-v1/*.root",
+	)
+	cfg = configtools.expand(cfg, ['nocuts', 'zcuts', 'noalphanoetacuts', 'noalphacuts', 'noetacuts', 'finalcuts'], ['None', 'L1', 'L1L2L3', 'L1L2L3Res'])
+	configtools.remove_quantities(cfg, ['jet1btag', 'jet1qgtag', 'jet1ptl1l2l3', 'jet1res', 'jet1rc'])
+	return cfg
