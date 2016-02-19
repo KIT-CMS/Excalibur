4fcb57578fbf1bc5fb6c7e22032883f5941d9bc3
cheidecker <christoph.heidecker@student.kit.edu>
2016-02-19 13:04:37 +0100

    Changed GeneratorWeight and NumberOfGeneratedEvents

--- cfg/excalibur/mc15_25ns.py
+++ cfg/excalibur/mc15_25ns.py
@@ -13,6 +13,6 @@ def config():
 	configtools.remove_quantities(cfg, ['jet1btag', 'jet1qgtag', 'jet1rc'])
 #	cfg['NumberGeneratedEvents'] = 9056998 #for: Zmm_Zmm_DYJetsToLL_M-50_madgraphMLM-pythia8_25ns
 	cfg['NumberGeneratedEvents'] = 29193937 #for: Zmm_Zmm_DYJetsToLL_M-50_amcatnloFXFX-pythia8_HCALDebug_25ns
-#	cfg['GeneratorWeight'] =  0.665689483222 #for: Zmm_Zmm_DYJetsToLL_M-50_amcatnloFXFX-pythia8_HCALDebug_25ns
+	cfg['GeneratorWeight'] =  0.665689483222 #for: Zmm_Zmm_DYJetsToLL_M-50_amcatnloFXFX-pythia8_HCALDebug_25ns
 	cfg['CrossSection'] = 6025.2  # https://twiki.cern.ch/twiki/bin/viewauth/CMS/StandardModelCrossSectionsat13TeV
 	return cfg
--- cfg/excalibur/mc15_25ns_ee.py
+++ cfg/excalibur/mc15_25ns_ee.py
@@ -13,6 +13,6 @@ def config():
 	configtools.remove_quantities(cfg, ['jet1btag', 'jet1qgtag', 'jet1rc', "e1mvanontrig", "e1mvatrig", "e2mvanontrig", "e2mvatrig"])
 #	cfg['NumberGeneratedEvents'] = 9056998 #for: Zee_Zee_DYJetsToLL_M-50_madgraphMLM-pythia8_25ns
 	cfg['NumberGeneratedEvents'] = 29193937 #for: Zee_Zee_DYJetsToLL_M-50_amcatnloFXFX-pythia8_HCALDebug_25ns
-#	cfg['GeneratorWeight'] =  0.65015166151 #for: Zee_Zee_DYJetsToLL_M-50_amcatnloFXFX-pythia8_HCALDebug_25ns
+	cfg['GeneratorWeight'] =  0.65015166151 #for: Zee_Zee_DYJetsToLL_M-50_amcatnloFXFX-pythia8_HCALDebug_25ns
 	cfg['CrossSection'] = 6025.2  # https://twiki.cern.ch/twiki/bin/viewauth/CMS/StandardModelCrossSectionsat13TeV
 	return cfg
