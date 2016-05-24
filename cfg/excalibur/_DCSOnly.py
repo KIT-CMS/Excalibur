3747d9563deed5d70c9b5bcb8ccfba8c395d0415
cheidecker <christoph.heidecker@student.kit.edu>
2016-05-24 15:46:40 +0200

    preparing for new data

--- cfg/excalibur/_DCSOnly.py
+++ cfg/excalibur/_DCSOnly.py
@@ -4,5 +4,5 @@ def modify_config(cfg):
 	"""
 	Use runs from DCSONLY Json
 	"""
-	cfg['JsonFiles'].set_base_json("/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/DCSOnly/json_DCSONLY.txt")
+	cfg['JsonFiles'].set_base_json("/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/DCSOnly/json_DCSONLY.txt")
 	return cfg
---
+++ cfg/excalibur/data16_25ns.py
@@ -0,0 +1,12 @@
+import configtools
+
+
+def config():
+	cfg = configtools.getConfig('data', 2015, 'mm', bunchcrossing='25ns')
+	cfg["InputFiles"].set_input(
+#		ekppath="/storage/a/cheidecker/cmssw807_calo_freiburg/Zll_DoMuRun2016B-PromptReco-v1/*.root",
+		ekppath="/storage/a/cheidecker/cmssw807_calo_naf/Zll_DoMuRun2016B-PromptReco-v1/*.root",
+	)
+	cfg = configtools.expand(cfg, ['nocuts', 'zcuts', 'noalphanoetacuts', 'noalphacuts', 'noetacuts', 'finalcuts'], ['None', 'L1', 'L1L2L3', 'L1L2L3Res'])
+	configtools.remove_quantities(cfg, ['jet1btag', 'jet1qgtag', 'jet1ptl1l2l3', 'jet1res', 'jet1rc'])
+	return cfg
