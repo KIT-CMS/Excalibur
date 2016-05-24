677e29356871469561e2d7d3c28ae819b61d90fe
dhaitz <dhaitz@cern.ch>
2016-05-24 15:48:31 +0200

    remove deprecated scripts

--- scripts/get_weights.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ALGO=${ALGO:-ak4PFJetsCHS}
-QUANTITY=${QUANTITY:-npv}
-BINNING=${BINNING:-"30,0.5,30.5"}
-echo "(npv=="`merlin.py -x $QUANTITY -i $1 $2 --zjetf nocuts --corr L1L2L3 --algo $ALGO --analy NormalizeToFirstHisto Ratio --nicks-white ratio --log-l debug --x-bins $BINNING --y-lims 0 1 | grep fSumw | cut -d "=" -f 2 | cut -d "," -f 1 | while read i; do echo ")*${i}+(npv=="; done | nl | sed 's/ //g' | sed 's/\t//g' ` | sed 's/ //g' | sed 's/.\{7\}$//'
