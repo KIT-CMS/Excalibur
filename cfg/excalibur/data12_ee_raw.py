7872e6bef5b2242d196ccbbff76ae144aa84e025
dhaitz <dhaitz@cern.ch>
2016-05-24 15:39:27 +0200

    remove obsolete cfg files

--- cfg/excalibur/data12_ee_raw.py
+++
@@ -1,7 +0,0 @@
-import configtools
-import data12_ee
-
-def config():
-	cfg = data12_ee.config()
-	cfg['Electrons'] = 'electrons'
-	return cfg
--- cfg/excalibur/mc12_ee_raw.py
+++
@@ -1,7 +0,0 @@
-import configtools
-import mc12_ee
-
-def config():
-	cfg = mc12_ee.config()
-	cfg['Electrons'] = 'electrons'
-	return cfg
