6d2dff1467be60607d95004a4b91b65a35f80e5e
cheidecker <christoph.heidecker@student.kit.edu>
2016-05-30 16:22:21 +0200

    new skims

---
+++ cfg/excalibur/data16_25ns_ee.py
@@ -0,0 +1,13 @@
+import configtools
+
+
+def config():
+	cfg = configtools.getConfig('data', 2015, 'ee', bunchcrossing='25ns')
+	cfg["InputFiles"].set_input(
+#		ekppath="/storage/a/cheidecker/cmssw807_calo_freiburg/Zll_DoMuRun2016B-PromptReco-v1/*.root",
+#		ekppath="/storage/a/wayand/data2016_nopujetid_v1/DoubleEG/crab_Zll_DoElRun2016B-PromptReco-v1/160524_222650/0000/*.root",
+		ekppath="/storage/a/cheidecker/cmssw807_calo_noPUJetID/Zll_DoElRun2016B-PromptReco-v2/*.root",
+	)
+	cfg = configtools.expand(cfg, ['nocuts', 'zcuts', 'noalphanoetacuts', 'noalphacuts', 'noetacuts', 'finalcuts'], ['None', 'L1', 'L1L2L3', 'L1L2L3Res'])
+	configtools.remove_quantities(cfg, ['jet1btag', 'jet1qgtag', 'jet1ptl1l2l3', 'jet1res', 'jet1rc'])
+	return cfg
