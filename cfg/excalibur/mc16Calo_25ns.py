05d5c097b3dafa31bcc96b5e9e92f23f4966ec3b
Anna Friedel <afriedel@ekpcms6.physik.uni-karlsruhe.de>
2016-05-31 14:09:23 +0200

    Configs for CaloJets

---
+++ cfg/excalibur/mc16Calo_25ns.py
@@ -0,0 +1,16 @@
+import configtools
+
+
+def config():
+	cfg = configtools.getConfig('mc', 2015, 'mm', bunchcrossing='25ns')
+	cfg["InputFiles"].set_input(
+		ekppath="/storage/a/cheidecker/cmssw807_calo_noPUJetID/Zll_DYJetsToLL_M-50_amcatnloFXFX-pythia8_25ns_v7/*.root",
+	)
+	cfg['RC'] = False #Disable because not available for Calo
+	cfg['TaggedJets'] = 'ak4CaloJets'
+	cfg = configtools.expand(cfg, ['nocuts', 'zcuts', 'noalphanoetacuts', 'noalphacuts', 'noetacuts', 'finalcuts'], ['None', 'L1', 'L1L2L3'])
+	configtools.remove_quantities(cfg, ['jet1btag', 'jet1qgtag', 'jet1rc'])
+	cfg['NumberGeneratedEvents'] = 28874658 #for: Zll_DYJetsToLL_M-50_amcatnloFXFX-pythia8_25ns_v7
+#	cfg['GeneratorWeight'] =  0.0 #for: Zll_DYJetsToLL_M-50_amcatnloFXFX-pythia8_25ns_v7
+	cfg['CrossSection'] = 6025.2  # https://twiki.cern.ch/twiki/bin/viewauth/CMS/StandardModelCrossSectionsat13TeV
+	return cfg
