6272b4c35659dd44bcd7116aef483a32a6a4b29b
Anna Friedel <afriedel@ekpcms6.physik.uni-karlsruhe.de>
2016-06-07 15:14:52 +0200

    switched RC to True again

--- cfg/python/defaultconfig.py
+++ cfg/python/defaultconfig.py
@@ -15,7 +15,7 @@ def getBaseConfig(tagged=True, **kwargs):
 		# ZJetCorrectionsProducer Settings
 		'Jec': '', # Path for JEC data, please set this later depending on input type
 		'L1Correction': 'L1FastJet',
-		'RC': 'False',  # Also provide random cone offset JEC, and use for type-I
+		'RC': False,  # Also provide random cone offset JEC, and use for type-I
 		'FlavourCorrections': False,  # Calculate additional MC flavour corrections
 		# ZProducer Settings
 		'ZMassRange': 20.,
