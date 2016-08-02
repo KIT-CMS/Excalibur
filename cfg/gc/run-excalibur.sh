615d5fdc5f697fa45b7f1ce53c9868255062d2c5
cheidecker <christoph.heidecker@student.kit.edu>
2016-08-02 15:12:53 +0200

    fix Artus crash without Excalibur failure

--- cfg/gc/run-excalibur.sh
+++ cfg/gc/run-excalibur.sh
@@ -32,15 +32,15 @@ echo "#### Starting Artus..."
 _TME_FILE='artus.tme'
 if [[ $(which time) ]]; then
 	# GNU time allows for extended format - use IEEE POSIX.2 as base
-	$(which time) -o ${_TME_FILE} -f 'real %e\nuser %U\nsys %S\nrss %M\nmajor_pfault %F\nmajor_pfault%R\nwaits %w' @WORKPATH@/artus @ARTUS_CONFIG@
+	$(which time) -o ${_TME_FILE} -f 'real %e\nuser %U\nsys %S\nrss %M\nmajor_pfault %F\nmajor_pfault%R\nwaits %w' @WORKPATH@/artus @ARTUS_CONFIG@ || exit 1
 elif [[ $(type time) ]]; then
 	# use bash's time in POSIX.2 mode, clone pipes to separate streams
 	exec 3>&1
 	exec 4>&2
-	{ time -p @WORKPATH@/artus @ARTUS_CONFIG@ 1>&3 2>&4;} 1>${_TME_FILE} 2>&1
+	{ time -p @WORKPATH@/artus @ARTUS_CONFIG@ 1>&3 2>&4;} 1>${_TME_FILE} 2>&1 || exit 1
 else
 	echo "# no 'time' found" 1>&2
-	@WORKPATH@/artus @ARTUS_CONFIG@
+	@WORKPATH@/artus @ARTUS_CONFIG@ || exit 1
 fi
 
 echo "#### Artus Performance"
