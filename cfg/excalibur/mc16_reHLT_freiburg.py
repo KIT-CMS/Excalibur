db01d67d6c80f86e15126d813b5e02791188aaea
Anna Friedel <afriedel@ekpbms2.ekp.kit.edu>
2017-01-12 10:44:51 +0100

    added nocuts to analyze flags

--- cfg/excalibur/mc16_reHLT_freiburg.py
+++ cfg/excalibur/mc16_reHLT_freiburg.py
@@ -8,7 +8,7 @@ def config():
 	
 		ekppath="srm://dgridsrm-fzk.gridka.de:8443/srm/managerv2?SFN=/pnfs/gridka.de/dcms/disk-only/store/user/afriedel/Skimming/mc-Iso04/MC_DYJets_to_LL/*.root"
 	)	
-	cfg = configtools.expand(cfg, ['zcuts',  'leptoncuts', 'genzcuts', 'genleptoncuts', 'allzcuts', 'allleptoncuts'], ['None'])
+	cfg = configtools.expand(cfg, ['zcuts',  'leptoncuts', 'genzcuts', 'genleptoncuts', 'allzcuts', 'allleptoncuts', 'nocuts'], ['None'])
 	configtools.remove_quantities(cfg, ['jet1btag', 'jet1qgtag', 'jet1rc'])
 	cfg['NumberGeneratedEvents'] = 104083466 #for:  /DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIIFall15DR76-PU25nsData2015v1_76X_mcRun2_asymptotic_v12_ext4-v1/AODSIM
 	cfg['GeneratorWeight'] =  0.670104921874 #for: Zll_DYJetsToLL_M-50_amcatnloFXFX-pythia8_ext4_25ns
