135e04f71c191d7da288e38405bb2ad9f15e6142
Thomas Berger <tberger@ekpbms2.ekp.kit.edu>
2017-01-18 13:30:16 +0100

    added mc-cfg

---
+++ cfg/excalibur/mc16_JECv2_CHS_ee.py
@@ -0,0 +1,19 @@
+import configtools
+
+
+def config():
+	cfg = configtools.getConfig('mc', 2016, 'ee', bunchcrossing='25ns')
+	cfg["InputFiles"].set_input(
+		ekppath="/storage/jbod/tberger/SkimmingResults/Zll_DYJetsToLL_M-50_amcatnloFXFX-Summer16-pythia8_25ns/Zll_DYJetsToLL_M-50_amcatnloFXFX-pythia8_RunIISummer16/*.root"
+		#ekppath="srm://grid-srm.physik.rwth-aachen.de:8443/srm/managerv2?SFN=/pnfs/physik.rwth-aachen.de/cms/store/user/tberger/Zll_DYJetsToLL_M-50_amcatnloFXFX-pythia8_RunIISummer16/*.root"
+		#ekppath="srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/tberger/Skimming/dataminiaod_BC_2016-10-24/Zll_DYJetsToLL_M-50_amcatnloFXFX-pythia8_25nsv2_v0-v1/*.root"				
+		#ekppath="/storage/jbod/tberger/SkimmingResults/Zll_DYJetsToLL_M-50_amcatnloFXFX-pythia8_25nsv2_v0-v1/*.root"
+		#nafpath="//pnfs/desy.de/cms/tier2/store/user/tberger/Skimming/dataminiaod_BC_2016-10-24/Zll_DYJetsToLL_M-50_amcatnloFXFX-pythia8_25nsv2_v0-v1/*.root"
+	)
+	#cfg['JsonFiles'] = [configtools.getPath() + 'data/json/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt']
+	#cfg['Jec'] = configtools.getPath() + '/data/JECDatabase/textFiles/Summer16_23Sep2016V2_MC/Summer16_23Sep2016V2_MC'
+	configtools.remove_quantities(cfg, ['jet1btag', 'jet1qgtag', 'jet1rc'])
+	cfg['NumberGeneratedEvents'] = 28611654 # for full Set: 28696958   
+	cfg['GeneratorWeight'] =  0.670201351233 #for: Zll_DYJetsToLL_M-50_amcatnloFXFX-pythia8_25nsv2_v0-v1
+	cfg['CrossSection'] = 6025.2
+	return cfg
