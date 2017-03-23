2ecd394c457edcd07fe7caba2a2b722425ced166
Anna Friedel <afriedel@nafhh-cms02.desy.de>
2017-03-23 14:58:34 +0100

    Settings for MinPt and Max Eta

--- Compile/src/Producers/ZJetGenParticleProducer.cc
+++ Compile/src/Producers/ZJetGenParticleProducer.cc
@@ -23,52 +23,41 @@ unsigned int ZJetGenParticleProducer::FindMom(unsigned int idx, ZJetEvent const&
 	
 
 }
-
 void ZJetGenParticleProducer::Produce(ZJetEvent const& event, ZJetProduct& product,
                      ZJetSettings const& settings) const
 {
 	unsigned int idx = 0;
 	unsigned int mom_idx = 0;
 	unsigned int daughter_idx = 0;
-	/*for (KGenParticles::iterator genParticle = event.m_genParticles->begin();
-	     genParticle != event.m_genParticles->end(); ++genParticle)
-	{
-		std::cout << idx << "\t" << genParticle->pdgId << "\t" << genParticle->status() << "\t" << std::setprecision(3) << genParticle->p4.Pt()<<  "\t" << std::setprecision(3) << "\t(";
-				for (std::vector<unsigned int>::const_iterator decayParticleIndex = genParticle->daughterIndices.begin();
-		     decayParticleIndex != genParticle->daughterIndices.end(); ++decayParticleIndex)
-					std::cout << *decayParticleIndex << " ";
-				std::cout << ")" << std::endl;
-	idx++;
-	}
-	idx = 0;
-	std::cout << "_________________________________________________________________" << std::endl;*/
+	std::vector<std::pair<unsigned int, float>> muons;
 	for (KGenParticles::iterator genParticle = event.m_genParticles->begin();
 	     genParticle != event.m_genParticles->end(); ++genParticle)
 	{
 		if ((std::abs(genParticle->pdgId) == PdgId)&&(genParticle->status() == 1))
 		{
 			mom_idx = idx;
-			if(this->PassKinematicCuts(&(*event.m_genParticles).at(idx), event, product)){ //This does not work properly...why?
-				//std::cout << "Added to GenMuons:" << idx << std::endl;
-				if((*event.m_genParticles).at(idx).p4.Pt() < 27 || std::abs((*event.m_genParticles).at(idx).p4.Eta()) > 2.3){
-					//std::cout << "This should not happen if your config set pt cut to 27 GeV and eta to 2.3!" << std::endl;
+			while(std::abs((*event.m_genParticles).at(mom_idx).pdgId) == PdgId){
+				daughter_idx = mom_idx;	
+				mom_idx = FindMom(daughter_idx, event);
+			}
+			if((*event.m_genParticles).at(daughter_idx).isPrompt()&&(*event.m_genParticles).at(daughter_idx).isHardProcess()){
+				if((*event.m_genParticles).at(idx).p4.Pt() > std::stof(settings.GetGenMuonLowerPtCuts()[0]) && std::abs((*event.m_genParticles).at(idx).p4.Eta()) < std::stof(settings.GetGenMuonUpperAbsEtaCuts()[0])){
+					float value = (float) (*event.m_genParticles).at(daughter_idx).p4.Pt();
+					muons.push_back(std::make_pair(idx, value));
 				}
-				else{
-					while(std::abs((*event.m_genParticles).at(mom_idx).pdgId) == PdgId){
-						daughter_idx = mom_idx;	
-						mom_idx = FindMom(daughter_idx, event);
-						//std::cout << "Search Mother for: " << daughter_idx << " found: " << mom_idx << std::endl;
-					}
-					if(std::abs(((*event.m_genParticles).at(mom_idx).pdgId) != DefaultValues::pdgIdTau)){
-						//std::cout << "Added to ValidMuons:" << daughter_idx << " with Mother: " << mom_idx << std::endl;
-						product.m_genMuons.push_back(&(*event.m_genParticles).at(idx)); //Put status 1 muons in m_genMuons 
-						(product.*leptons).push_back(&(*event.m_genParticles).at(daughter_idx)); //Put original muons in m_validGenMuons
-					}
+				if((*event.m_genParticles).at(daughter_idx).p4.Pt() > std::stof(settings.GetGenMuonLowerPtCuts()[0]) && std::abs((*event.m_genParticles).at(daughter_idx).p4.Eta()) < std::stof(settings.GetGenMuonUpperAbsEtaCuts()[0])){
+					(product.*leptons).push_back(&(*event.m_genParticles).at(daughter_idx)); //Put original muons in m_validGenMuons
 				}
 			}
 
 		}
-			idx++;
+		idx++;
+	}
+	std::sort((product.*leptons).begin(), (product.*leptons).end(),
+			[](KGenParticle const* lepton1, KGenParticle const* lepton2) -> bool
+			{ return lepton1->p4.Pt() > lepton2->p4.Pt(); });
+	std::sort(muons.begin(),muons.end(),[](std::pair<unsigned int,float> a, std::pair<unsigned int,float> b) -> bool{return a.second > b.second;});
+	for (unsigned int i = 0; i < muons.size(); i++){
+		product.m_genMuons.push_back(&(*event.m_genParticles).at(muons[i].first));
 	}
 }
-
