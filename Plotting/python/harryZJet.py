0ad2eeb3a0d8fc8961b14b21521fe48087aa872e
Anna Friedel <anna.friedel@cern.ch>
2017-05-05 12:19:20 +0200

    Make merlin work again with current Artus

--- Plotting/python/harryZJet.py
+++ Plotting/python/harryZJet.py
@@ -7,6 +7,7 @@ import Artus.HarryPlotter.harry as harry
 
 class HarryPlotterZJet(harry.HarryPlotter):
 
-	def plot(self, harry_args):
+	def plot(self, plot_index):
+		harry_args = self.harry_args[plot_index]
 		harry_core_zjet = coreZJet.HarryCoreZJet(args_from_script=harry_args)
 		return harry_core_zjet.run()
