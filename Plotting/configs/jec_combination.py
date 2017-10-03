e53fe9cc7ac9ff2df64954acf8bfa5f53111efca
Berger Thomas <thomas.berger@kit.edu>
2017-10-03 13:53:14 +0200

    jec_combinations up to date

--- Plotting/configs/jec_combination.py
+++ Plotting/configs/jec_combination.py
@@ -245,62 +245,6 @@ def jec_pu_combination(args=None, additional_dictionary=None, algo='CHS'):
 	harryinterface.harry_interface(mpl_plots, args)
 	harryinterface.harry_interface(root_plots, args + ['--max-processes', '1'])
 
-
-
-
-
-def jec_combination_20150722(args=None):
-	d = {
-		'files': [
-			'ntuples/Data_13TeV_74X_E2_50ns_2015-07-22.root',
-			'ntuples/MC_13TeV_74X_E2_50ns_algo_2015-07-22.root',
-		],
-		"algorithms": ["ak4PFJetsCHS",],
-		# NPV hardcoded from Dominik's ``get_weights`` script output @ 20150722
-		"_npv_weights" : ["1", "(npv==1)*60.7406+(npv==2)*4.00814+(npv==3)*5.16789+(npv==4)*4.87128+(npv==5)*4.18218+(npv==6)*3.43252+(npv==7)*3.26353+(npv==8)*2.78556+(npv==9)*2.65932+(npv==10)*2.02964+(npv==11)*1.64236+(npv==12)*1.30764+(npv==13)*1.17253+(npv==14)*0.901506+(npv==15)*0.687614+(npv==16)*0.636555+(npv==17)*0.366818+(npv==18)*0.318782+(npv==19)*0.21218+(npv==20)*0.148066+(npv==21)*0.104698+(npv==22)*0.0391752+(npv==23)*0.0121797+(npv==24)*0.0611739+(npv==25)*0.0192279+(npv==26)*0+(npv==27)*0+(npv==28)*0+(npv==29)*0+(npv==30)*0"],
-	}
-	jec_combination(args, d)
-
-
-def jec_combination_20150804(args=None):
-	d = {
-		'files': [
-			'ntuples/Data_13TeV_74X_E2_50ns_2015-07-31.root',
-			'ntuples/MC_13TeV_74X_E2_50ns_algo_2015-07-31.root',
-		],
-		"algorithms": ["ak4PFJetsCHS"],
-		# NPV hardcoded from Dominik's ``get_weights`` script output @ 20150804
-		"_npv_weights" : ["1", "(npv==1)*38.9021+(npv==2)*2.3337+(npv==3)*2.44477+(npv==4)*1.70175+(npv==5)*1.52866+(npv==6)*1.31595+(npv==7)*1.35314+(npv==8)*1.34328+(npv==9)*1.38119+(npv==10)*1.28674+(npv==11)*1.22246+(npv==12)*1.188+(npv==13)*1.14079+(npv==14)*1.13437+(npv==15)*1.10264+(npv==16)*1.0896+(npv==17)*0.953372+(npv==18)*0.917887+(npv==19)*0.739569+(npv==20)*0.707311+(npv==21)*0.637729+(npv==22)*0.570234+(npv==23)*0.471585+(npv==24)*0.391797+(npv==25)*0.279881+(npv==26)*0.247282+(npv==27)*0.171765+(npv==28)*0.146706+(npv==29)*0.150545+(npv==30)*0.0927622"],
-	}
-	jec_combination(args, d)
-
-
-def jec_combination_20150814(args=None):
-	d = {
-		'files': [
-			'ntuples/Data_13TeV_74X_E2_50ns_JECSummerV4_2015-08-14.root',
-			'ntuples/MC_13TeV_74X_E2_50ns_JECSummerV4_2015-08-14.root',
-		],
-		"algorithms": ["ak4PFJetsCHS"],
-		# NPV hardcoded from Dominik's ``get_weights`` script output @ 20150814
-		"_npv_weights" : ["1", "(npv==1)*14.8846+(npv==2)*3.58332+(npv==3)*1.84285+(npv==4)*2.45195+(npv==5)*1.50428+(npv==6)*0.996686+(npv==7)*1.34553+(npv==8)*1.26528+(npv==9)*1.21698+(npv==10)*1.20916+(npv==11)*1.14444+(npv==12)*1.09916+(npv==13)*1.1722+(npv==14)*1.11612+(npv==15)*1.202+(npv==16)*1.12021+(npv==17)*1.00273+(npv==18)*1.00425+(npv==19)*0.768872+(npv==20)*0.785899+(npv==21)*0.649426+(npv==22)*0.714884+(npv==23)*0.534425+(npv==24)*0.278714+(npv==25)*0.333719+(npv==26)*0.28414+(npv==27)*0.0830113+(npv==28)*0.171738+(npv==29)*0.145598+(npv==30)*0.13861"],
-	}
-	jec_combination(args, d)
-
-
-def jec_combination_25ns_20151001(args=None):
-	d = {
-		'files': [
-			'ntuples/Data_13TeV_74X_E2_25ns_JECV3_2015D_2015-10-01.root',
-			'ntuples/MC_13TeV_74X_E2_25ns_JECV3_2015-10-01.root',
-		],
-		"algorithms": ["ak4PFJetsCHS"],
-		# NPV hardcoded from Dominik's ``get_weights`` script output @ 20151001
-		"_npv_weights" : ["(weight)", "(weight)*((npv==1)*6.9295+(npv==2)*4.58177+(npv==3)*3.68256+(npv==4)*3.39719+(npv==5)*3.06555+(npv==6)*2.74328+(npv==7)*2.418+(npv==8)*2.02356+(npv==9)*1.71068+(npv==10)*1.37638+(npv==11)*1.07853+(npv==12)*0.814396+(npv==13)*0.60545+(npv==14)*0.443644+(npv==15)*0.322044+(npv==16)*0.215438+(npv==17)*0.145175+(npv==18)*0.104847+(npv==19)*0.0769262+(npv==20)*0.0508476+(npv==21)*0.0331774+(npv==22)*0.0311605+(npv==23)*0.0153405+(npv==24)*0.0131355+(npv==25)*0.00283383+(npv==26)*0.0132552+(npv==27)*0+(npv==28)*0+(npv==29)*0+(npv==30)*0)"],
-	}
-	jec_combination(args, d)
-
-
 def jec_combination_25ns_20151016(args=None):
 	plots = []
 	mc_file = 'ntuples/MC_13TeV_74X_E2_25ns_2015-10-18.root'
@@ -322,56 +266,135 @@ def jec_combination_25ns_20151016(args=None):
 	for d in plots:
 		jec_combination(args, d)
 
-def jec_combination_20151204_Zee(args=None):
-	d = {
-		'files': [
-			'ntuples/Data_Zee_13TeV_74X_E2_25ns_2015-12-04.root',
-			'ntuples/MC_Zee_13TeV_74X_E2_25ns_2015-12-04.root',
-		],
-		"algorithms": ["ak4PFJetsCHS",],
-	}
-	jec_combination(args, d)
+FOLDER='/storage/jbod/tberger/zjets/excalibur_results_datamc_Summer16_03Feb2017_V7/'
 
-def jec_combination_20160607_PUPPI_Zmm(args=None):
+def jec_combination_CHS_Zmm_BCD(args=None):
 	d = {
 		'files': [
-			'work/data16Puppi_new_JSON.root',
-			'work/mc15Puppi_25ns_80.root',
-		],
-		"algorithms": ["ak4PFJetsPuppi"],
-	}
-	jec_combination(args, d, 'Puppi')
-
-def jec_combination_20160607_PUPPI_Zee(args=None):
-	d = {
-		'files': [
-			'work/data16Puppi_new_JSON_ee.root',
-			'work/mc15Puppi_25ns_ee_80.root',
-		],
-		"algorithms": ["ak4PFJetsPuppi"],
-	}
-	jec_combination(args, d, 'Puppi')
-
-def jec_combination_CHS_Zmm(args=None):
-	#for run2 2016 CHS jec combination files using jec Spring16_25nsV3
-	d = {
-		'files': [
-			'/portal/ekpcms6/home/cheidecker/Excalibur/work/data16_25ns.root',
-			'/portal/ekpcms6/home/cheidecker/Excalibur/work/mc16_25ns.root',
+			FOLDER+'data16_BCD_mm_remini.root',
+			FOLDER+'mc16_BCDEFGH_mm_madgraph_NJ.root',
 		],
 		#"algorithms": ["ak4PFJetsCHS"],
-		"www": 'jec_combination_CHS_Zmm_fullalpha',
+		"output_dir": 'plots_'+time.strftime("%Y_%m_%d", time.localtime())+'/combination_Zmm_BCD_remini_madgraph_NJ',
+		'file_label' : 'Zmm_BCD_',
+#	 	"www": 'jec_combination_CHS_Zmm_fullalpha',
 	}
 	jec_combination(args, d, 'CHS')
 
-def jec_combination_CHS_Zee(args=None):
-	#for run2 2016 CHS jec combination files using jec Spring16_25nsV3
+def jec_combination_CHS_Zmm_EF(args=None):
 	d = {
 		'files': [
-			'/portal/ekpcms6/home/cheidecker/Excalibur/work/data16_25ns_ee.root',
-			'/portal/ekpcms6/home/cheidecker/Excalibur/work/mc16_25ns_ee.root',
+			FOLDER+'data16_EF_mm_remini.root',
+			FOLDER+'mc16_BCDEFGH_mm_madgraph_NJ.root',
 		],
 		#"algorithms": ["ak4PFJetsCHS"],
-		"www": 'jec_combination_CHS_Zee_fullalpha',
+		'file_label' : 'Zmm_EF_',
+		"output_dir": 'plots_'+time.strftime("%Y_%m_%d", time.localtime())+'/combination_Zmm_EF_remini_madgraph_NJ',
+#		"www": 'jec_combination_CHS_Zmm_fullalpha',
 	}
 	jec_combination(args, d, 'CHS')
+
+def jec_combination_CHS_Zmm_G(args=None):
+	d = {
+		'files': [
+			FOLDER+'data16_G_mm_remini.root',
+			FOLDER+'mc16_BCDEFGH_mm_madgraph_NJ.root',
+		],
+		#"algorithms": ["ak4PFJetsCHS"],
+		'file_label' : 'Zmm_G_',
+		"output_dir": 'plots_'+time.strftime("%Y_%m_%d", time.localtime())+'/combination_Zmm_G_remini_madgraph_NJ',
+#		"www": 'jec_combination_CHS_Zmm_fullalpha',
+	}
+	jec_combination(args, d, 'CHS')
+
+def jec_combination_CHS_Zmm_H(args=None):
+	d = {
+		'files': [
+			FOLDER+'data16_H_mm_remini.root',
+			FOLDER+'mc16_BCDEFGH_mm_madgraph_NJ.root',
+		],
+		#"algorithms": ["ak4PFJetsCHS"],
+		'file_label' : 'Zmm_H_',
+		"output_dir": 'plots_'+time.strftime("%Y_%m_%d", time.localtime())+'/combination_Zmm_H_remini_madgraph_NJ',
+#		"www": 'jec_combination_CHS_Zmm_fullalpha',
+	}
+	jec_combination(args, d, 'CHS')
+
+def jec_combination_CHS_Zmm_BCDEFGH(args=None):
+	d = {
+		'files': [
+			FOLDER+'data16_BCDEFGH_mm_remini.root',
+			FOLDER+'mc16_BCDEFGH_mm_madgraph_NJ.root',
+		],
+		#"algorithms": ["ak4PFJetsCHS"],
+		'file_label' : 'Zmm_BCDEFGH_',
+		"output_dir": 'plots_'+time.strftime("%Y_%m_%d", time.localtime())+'/combination_Zmm_BCDEFGH_remini_madgraph_NJ',
+#		"www": 'jec_combination_CHS_Zmm_fullalpha',
+	}
+	jec_combination(args, d, 'CHS')
+		
+def jec_combination_CHS_Zee_BCD(args=None):
+	d = {
+		'files': [
+			FOLDER+'data16_BCD_ee_remini.root',
+			FOLDER+'mc16_BCDEFGH_ee_madgraph_NJ.root',
+		],
+		#"algorithms": ["ak4PFJetsCHS"],
+		'file_label' : 'Zee_BCD_',
+		"output_dir": 'plots_'+time.strftime("%Y_%m_%d", time.localtime())+'/combination_Zee_BCD_remini_madgraph_NJ',
+#		"www": 'jec_combination_CHS_Zee_fullalpha',
+	}
+	jec_combination(args, d, 'CHS')
+	
+def jec_combination_CHS_Zee_EF(args=None):
+	d = {
+		'files': [
+			FOLDER+'data16_EF_ee_remini.root',
+			FOLDER+'mc16_BCDEFGH_ee_madgraph_NJ.root',
+		],
+		#"algorithms": ["ak4PFJetsCHS"],
+		'file_label' : 'Zee_EF_',
+		"output_dir": 'plots_'+time.strftime("%Y_%m_%d", time.localtime())+'/combination_Zee_EF_remini_madgraph_NJ',
+#		"www": 'jec_combination_CHS_Zee_fullalpha',
+	}
+	jec_combination(args, d, 'CHS')
+
+def jec_combination_CHS_Zee_G(args=None):
+	d = {
+		'files': [
+			FOLDER+'data16_G_ee_remini.root',
+			FOLDER+'mc16_BCDEFGH_ee_madgraph_NJ.root',
+		],
+		#"algorithms": ["ak4PFJetsCHS"],
+		'file_label' : 'Zee_G_',
+		"output_dir": 'plots_'+time.strftime("%Y_%m_%d", time.localtime())+'/combination_Zee_G_remini_madgraph_NJ',
+#		"www": 'jec_combination_CHS_Zee_fullalpha',
+	}
+	jec_combination(args, d, 'CHS')
+	
+def jec_combination_CHS_Zee_H(args=None):
+	d = {
+		'files': [
+			FOLDER+'data16_H_ee_remini.root',
+			FOLDER+'mc16_BCDEFGH_ee_madgraph_NJ.root',
+		],
+		#"algorithms": ["ak4PFJetsCHS"],
+		'file_label' : 'Zee_H_',
+		"output_dir": 'plots_'+time.strftime("%Y_%m_%d", time.localtime())+'/combination_Zee_H_remini_madgraph_NJ',
+#		"www": 'jec_combination_CHS_Zee_fullalpha',
+	}
+	jec_combination(args, d, 'CHS')
+	
+def jec_combination_CHS_Zee_BCDEFGH(args=None):
+	d = {
+		'files': [
+			FOLDER+'data16_BCDEFGH_ee_remini.root',
+			FOLDER+'mc16_BCDEFGH_ee_madgraph_NJ.root',
+		],
+		#"algorithms": ["ak4PFJetsCHS"],
+		'file_label' : 'Zee_BCDEFGH_',
+		"output_dir": 'plots_'+time.strftime("%Y_%m_%d", time.localtime())+'/combination_Zee_BCDEFGH_remini_madgraph_NJ',
+#		"www": 'jec_combination_CHS_Zee_fullalpha',
+	}
+	jec_combination(args, d, 'CHS')
+	
