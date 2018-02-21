002ce8b890141309de9ca351e5369a0a1aa7c41a
Daniel Savoiu <daniel.savoiu@cern.ch>
2018-02-21 17:21:17 +0100

    Restructure 'pileup' directory: data/MC weights now stored in 'mc_weights' subdirectory.

--- cfg/excalibur/jec17/mc17_DYJets_madgraph/mc17_mm_DY2J_Madgraph.py
+++ cfg/excalibur/jec17/mc17_DYJets_madgraph/mc17_mm_DY2J_Madgraph.py
@@ -14,7 +14,7 @@ def config():
     cfg['JsonFiles'] = [os.path.join(configtools.getPath(), 'data/json/Cert_294927-306462_13TeV_PromptReco_Collisions17_JSON.txt')]
     cfg['Jec'] = os.path.join(configtools.getPath(), '../JECDatabase/textFiles/'+JEC+'_MC/'+JEC+'_MC')
     cfg = configtools.expand(cfg, ['nocuts','basiccuts','finalcuts'], ['None', 'L1', 'L1L2L3'])
-    cfg['PileupWeightFile'] = os.path.join(configtools.getPath() , 'data/pileup/mc/mc17_DYJets_madgraph/PUWeights_294927-306462_17Nov2017_DY2JetsToLL_Fall17-madgraphMLM_realistic_v10-v1.root')
+    cfg['PileupWeightFile'] = os.path.join(configtools.getPath() , 'data/pileup/mc_weights/mc17_DYJets_madgraph/PUWeights_BCDEF_17Nov2017_DY2JetsToLL_Fall17-madgraphMLM_realistic_v10-v1.root')
     cfg['NumberGeneratedEvents'] = 11623646
     cfg['GeneratorWeight'] = 1.0
     cfg['CrossSection'] = 332.8*1.23  # from mc16 config
