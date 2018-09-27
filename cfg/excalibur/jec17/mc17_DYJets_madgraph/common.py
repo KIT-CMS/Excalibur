abf05eec5ecf0d5f607839fd415447087b70481d
cheidecker <christoph.heidecker@student.kit.edu>
2018-09-27 13:52:00 +0200

    added xrootd path prefix

--- cfg/excalibur/jec17/mc17_DYJets_madgraph/common.py
+++ cfg/excalibur/jec17/mc17_DYJets_madgraph/common.py
@@ -3,5 +3,6 @@ JEC_VERSION='V24'
 
 SE_PATH_PREFIXES = dict(
     srm_gridka_nrg="srm://cmssrm-kit.gridka.de:8443/srm/managerv2?SFN=/pnfs/gridka.de/cms/disk-only/store/user",
+    xrootd_gridka_nrg="root://cmsxrootd-redirectors.gridka.de//pnfs/gridka.de/cms/disk-only/store/user",
     local_gridka_nrg="/storage/gridka-nrg",
 )
