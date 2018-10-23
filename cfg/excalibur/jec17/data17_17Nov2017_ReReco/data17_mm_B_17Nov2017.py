a110e8f315d41089a789b0f71b636ee0d8425a4d
cheidecker <christoph.heidecker@student.kit.edu>
2018-10-23 18:25:22 +0200

    Changed to xrootd paths

--- cfg/excalibur/jec17/data17_17Nov2017_ReReco/data17_mm_B_17Nov2017.py
+++ cfg/excalibur/jec17/data17_17Nov2017_ReReco/data17_mm_B_17Nov2017.py
@@ -14,8 +14,8 @@ JEC='{}{}_{}'.format(JEC_BASE, RUN, JEC_VERSION)
 def config():
     cfg = configtools.getConfig('data', 2017, CH, JEC=JEC)
     cfg["InputFiles"].set_input(
-        bmspathB="{}/dsavoiu/Skimming/ZJet_DoubleMuon_Run2017B-17Nov2017-v1-r2/*.root".format(SE_PATH_PREFIXES['srm_gridka_nrg']),
-        ekppathB="{}/dsavoiu/Skimming/ZJet_DoubleMuon_Run2017B-17Nov2017-v1-r2/*.root".format(SE_PATH_PREFIXES['srm_gridka_nrg']),
+        bmspathB="{}/dsavoiu/Skimming/ZJet_DoubleMuon_Run2017B-17Nov2017-v1-r2/*.root".format(SE_PATH_PREFIXES['xrootd_gridka_nrg']),
+        ekppathB="{}/dsavoiu/Skimming/ZJet_DoubleMuon_Run2017B-17Nov2017-v1-r2/*.root".format(SE_PATH_PREFIXES['xrootd_gridka_nrg']),
     )
     cfg['JsonFiles'] = [os.path.join(configtools.getPath(), 'data/json/Cert_294927-306462_13TeV_PromptReco_Collisions17_JSON.txt')]
 
