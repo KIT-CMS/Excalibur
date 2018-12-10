c475a0d6fff83855b878b0b458cfef24d6ad8b8e
Daniel Savoiu <daniel.savoiu@cern.ch>
2018-12-10 10:53:56 +0100

    [Summer16] [Fall17] Update configs for 09May2018 Re-Reco. Update JEC and JER smearing versions.

--- cfg/excalibur/jec17/data17_09May2018_ReReco/data17_mm_F_09May2018.py
+++ cfg/excalibur/jec17/data17_09May2018_ReReco/data17_mm_F_09May2018.py
@@ -1,20 +1,27 @@
 import configtools
 import os
+import sys
+
+# -- import common information
+sys.path.append(os.path.dirname(__file__))
+from common import JEC_BASE, JEC_VERSION, SE_PATH_PREFIXES
 
 RUN='F'
 CH='mm'
-JEC='Fall17_17Nov2017'+RUN+'_V10'
+JEC='{}{}_{}'.format(JEC_BASE, RUN, JEC_VERSION)
+
 
 def config():
-    cfg = configtools.getConfig('data', 2017, CH)
+    cfg = configtools.getConfig('data', 2017, CH, JEC=JEC)
     cfg["InputFiles"].set_input(
-        ekppathF="srm://cmssrm-kit.gridka.de:8443/srm/managerv2?SFN=/pnfs/gridka.de/cms/disk-only/store/user/dsavoiu/Skimming/ZJet_DoubleMuon_Run2017F-09May2018-v1/*.root",
-        #ekppathF="/storage/gridka-nrg/dsavoiu/Skimming/ZJet_DoubleMuon_Run2017F-09May2018-v1/*.root",
+        ekppathF="{}/dsavoiu/Skimming/ZJet_DoubleMuon_Run2017F-09May2018-v1/*.root".format(SE_PATH_PREFIXES['srm_gridka_nrg']),
+        bmspathF="{}/dsavoiu/Skimming/ZJet_DoubleMuon_Run2017F-09May2018-v1/*.root".format(SE_PATH_PREFIXES['srm_gridka_nrg']),
     )
     cfg['JsonFiles'] = [os.path.join(configtools.getPath(), 'data/json/Cert_294927-306462_13TeV_PromptReco_Collisions17_JSON.txt')]
-    cfg['Jec'] = os.path.join(configtools.getPath(), '../JECDatabase/textFiles/'+JEC+'_DATA/'+JEC+'_DATA')
+
     cfg['VertexSummary'] = 'offlinePrimaryVerticesSummary'
 
     cfg['ProvideL2ResidualCorrections'] = True
     cfg = configtools.expand(cfg, ['nocuts','basiccuts','finalcuts'], ['None', 'L1', 'L1L2L3', 'L1L2Res', 'L1L2L3Res'])
+
     return cfg
