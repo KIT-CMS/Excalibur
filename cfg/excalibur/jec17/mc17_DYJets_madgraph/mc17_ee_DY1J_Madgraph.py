db32fbbcb2ffa147d55a762c2852257f654b8b08
Christoph Heidecker <cheidecker@bms3.etp.kit.edu>
2019-01-28 10:26:07 +0100

    Changed path to new naming convention without hostname

--- cfg/excalibur/jec17/mc17_DYJets_madgraph/mc17_ee_DY1J_Madgraph.py
+++ cfg/excalibur/jec17/mc17_DYJets_madgraph/mc17_ee_DY1J_Madgraph.py
@@ -6,7 +6,6 @@ import sys
 sys.path.append(os.path.dirname(__file__))
 from common import JEC_BASE, JEC_VERSION, JER, SE_PATH_PREFIXES
 
-RUN='BCDEFGH'
 CH='ee'
 JEC='{}_{}'.format(JEC_BASE, JEC_VERSION)
 
@@ -14,8 +13,9 @@ JEC='{}_{}'.format(JEC_BASE, JEC_VERSION)
 def config():
     cfg = configtools.getConfig('mc', 2017, CH, JEC=JEC, JER=JER)
     cfg["InputFiles"].set_input(
-        bmspath1="{}/dsavoiu/Skimming/ZJet_DY1JetsToLL_Fall17-madgraphMLM_realistic_v10-v1/*.root".format(SE_PATH_PREFIXES['xrootd_gridka_nrg']),
-        ekppath1="{}/dsavoiu/Skimming/ZJet_DY1JetsToLL_Fall17-madgraphMLM_realistic_v10-v1/*.root".format(SE_PATH_PREFIXES['xrootd_gridka_nrg']),
+        path="{}/cheideck/Skimming/ZJet_DY1JetsToLL_Fall17-madgraphMLM_realistic_v10-v1/*.root".format(SE_PATH_PREFIXES['xrootd_gridka_nrg']),
+        # bmspath1="{}/dsavoiu/Skimming/ZJet_DY1JetsToLL_Fall17-madgraphMLM_realistic_v10-v1/*.root".format(SE_PATH_PREFIXES['xrootd_gridka_nrg']),
+        # ekppath1="{}/dsavoiu/Skimming/ZJet_DY1JetsToLL_Fall17-madgraphMLM_realistic_v10-v1/*.root".format(SE_PATH_PREFIXES['xrootd_gridka_nrg']),
     )
     cfg['JsonFiles'] = [os.path.join(configtools.getPath(), 'data/json/Cert_294927-306462_13TeV_PromptReco_Collisions17_JSON.txt')]
 
