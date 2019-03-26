e0a22a2de691f1e4a3a1e6c95772e36155661a49
Thomas Berger <thomas.berger@kit.edu>
2019-03-26 15:06:06 +0100

    smearing can also be applied on data (if you think this could be helpful)

--- Compile/src/Producers/JERSmearer.cc
+++ Compile/src/Producers/JERSmearer.cc
@@ -33,7 +33,10 @@ void JERSmearer::Init(ZJetSettings const& settings) {
 
     // get and validate smearing method configuration
     if (KappaTools::tolower(settings.GetJERMethod()) == "hybrid") {
-        m_smearingMethod = JERSmearer::SmearingMethod::HYBRID;
+        if (settings.GetInputIsData())
+            LOG(FATAL) << "[JERSmearer] JER smearing method hybrid not possible for data!!!";
+        else
+            m_smearingMethod = JERSmearer::SmearingMethod::HYBRID;
     }
     else if (KappaTools::tolower(settings.GetJERMethod()) == "stochastic") {
         m_smearingMethod = JERSmearer::SmearingMethod::STOCHASTIC;
@@ -56,6 +59,7 @@ void JERSmearer::Init(ZJetSettings const& settings) {
 
     // create the random number generator
     m_randomNumberGenerator = std::mt19937(settings.GetJERSmearerSeed());
+    std::cout << "test" << std::endl;
 }
 
 void JERSmearer::Produce(ZJetEvent const& event,
@@ -63,46 +67,72 @@ void JERSmearer::Produce(ZJetEvent const& event,
                          ZJetSettings const& settings) const {
 
     // iterate over all jet correction levels
-    for (std::map<std::string, std::vector<std::shared_ptr<KJet>>>::const_iterator itlevel = product.m_correctedZJets.begin();
-         itlevel != product.m_correctedZJets.end();
-         ++itlevel) {
-
-        // shortcuts for convenience
-        auto& recoJets = product.m_correctedZJets[itlevel->first];
-        const auto& matchedGenJetIndices = product.m_matchedGenJets[itlevel->first];
-
-        // go through all reco jets
-        for (size_t iJet = 0; iJet < recoJets.size(); ++iJet) {
-
-            // retrieve the (relative) pT resolution and the resolution scale factor
-            double jetResolution = m_jetResolution->getResolution({
-                {JME::Binning::JetPt, recoJets[iJet]->p4.Pt()},
-                {JME::Binning::JetEta, recoJets[iJet]->p4.Eta()},
-                {JME::Binning::Rho, event.m_pileupDensity->rho}
-            });
-            double jetResolutionScaleFactor = m_jetResolutionScaleFactor->getScaleFactor({
-                {JME::Binning::JetEta, recoJets[iJet]->p4.Eta()}
-            }, Variation::NOMINAL);
-
-            // get and validate matched gen jet
-            const KLV* matchedGenJet = nullptr;
-            if (matchedGenJetIndices[iJet] >= 0) {
-                matchedGenJet = product.m_simpleGenJets[matchedGenJetIndices[iJet]];
-
-                // invalidate match if additional pT-criterion is not met
-                if (std::abs(recoJets[iJet]->p4.Pt() - matchedGenJet->p4.Pt()) > 3 * jetResolution * recoJets[iJet]->p4.Pt()) {
-                    matchedGenJet = nullptr;
+    if (settings.GetInputIsData()) {
+        for (std::map<std::string, std::vector<std::shared_ptr<KJet>>>::const_iterator itlevel = product.m_correctedZJets.begin();
+             itlevel != product.m_correctedZJets.end();
+             ++itlevel) {
+    
+            // shortcuts for convenience
+            auto& recoJets = product.m_correctedZJets[itlevel->first];
+            // go through all reco jets
+            for (size_t iJet = 0; iJet < recoJets.size(); ++iJet) {
+                // retrieve the (relative) pT resolution and the resolution scale factor
+                double jetResolution = m_jetResolution->getResolution({
+                    {JME::Binning::JetPt, recoJets[iJet]->p4.Pt()},
+                    {JME::Binning::JetEta, recoJets[iJet]->p4.Eta()},
+                    {JME::Binning::Rho, event.m_pileupDensity->rho}
+                });
+                std::cout << jetResolution << std::endl;
+                double jetResolutionScaleFactor = m_jetResolutionScaleFactor->getScaleFactor({
+                    {JME::Binning::JetEta, recoJets[iJet]->p4.Eta()}
+                }, Variation::NOMINAL);
+                std::cout << jetResolutionScaleFactor << std::endl;
+                // compute and apply the pT smearing factor
+                recoJets[iJet]->p4 *= 1 + std::normal_distribution<>(0, jetResolution)(m_randomNumberGenerator);// * std::sqrt(std::max(jetResolutionScaleFactor * jetResolutionScaleFactor - 1, 0.0));
+            }
+        }
+    }
+    else {
+        for (std::map<std::string, std::vector<std::shared_ptr<KJet>>>::const_iterator itlevel = product.m_correctedZJets.begin();
+             itlevel != product.m_correctedZJets.end();
+             ++itlevel) {
+    
+            // shortcuts for convenience
+            auto& recoJets = product.m_correctedZJets[itlevel->first];
+            const auto& matchedGenJetIndices = product.m_matchedGenJets[itlevel->first];
+            // go through all reco jets
+            for (size_t iJet = 0; iJet < recoJets.size(); ++iJet) {
+                // retrieve the (relative) pT resolution and the resolution scale factor
+                double jetResolution = m_jetResolution->getResolution({
+                    {JME::Binning::JetPt, recoJets[iJet]->p4.Pt()},
+                    {JME::Binning::JetEta, recoJets[iJet]->p4.Eta()},
+                    {JME::Binning::Rho, event.m_pileupDensity->rho}
+                });
+                std::cout << jetResolution << std::endl;
+                double jetResolutionScaleFactor = m_jetResolutionScaleFactor->getScaleFactor({
+                    {JME::Binning::JetEta, recoJets[iJet]->p4.Eta()}
+                }, Variation::NOMINAL);
+                std::cout << jetResolutionScaleFactor << std::endl;
+                // get and validate matched gen jet
+                const KLV* matchedGenJet = nullptr;
+                if (matchedGenJetIndices[iJet] >= 0) {
+                    matchedGenJet = product.m_simpleGenJets[matchedGenJetIndices[iJet]];
+    
+                    // invalidate match if additional pT-criterion is not met
+                    if (std::abs(recoJets[iJet]->p4.Pt() - matchedGenJet->p4.Pt()) > 3 * jetResolution * recoJets[iJet]->p4.Pt()) {
+                        matchedGenJet = nullptr;
+                    }
                 }
-            }
-
-            // compute and apply the pT smearing factor
-            if ((matchedGenJet) && (m_smearingMethod == JERSmearer::SmearingMethod::HYBRID)) {
-                // match found and hybrid method requested -> smear using scaling method
-                recoJets[iJet]->p4 *= 1 + (jetResolutionScaleFactor - 1) * (recoJets[iJet]->p4.Pt() - matchedGenJet->p4.Pt())/recoJets[iJet]->p4.Pt();
-            }
-            else if ((m_smearingMethod == JERSmearer::SmearingMethod::STOCHASTIC) || (!matchedGenJet)) {
-                // match not found or stochastic method requested -> draw pT smearing factor from a normal distribution
-                recoJets[iJet]->p4 *= 1 + std::normal_distribution<>(0, jetResolution)(m_randomNumberGenerator) * std::sqrt(std::max(jetResolutionScaleFactor * jetResolutionScaleFactor - 1, 0.0));
+    
+                // compute and apply the pT smearing factor
+                if ((matchedGenJet) && (m_smearingMethod == JERSmearer::SmearingMethod::HYBRID)) {
+                    // match found and hybrid method requested -> smear using scaling method
+                    recoJets[iJet]->p4 *= 1 + (jetResolutionScaleFactor - 1) * (recoJets[iJet]->p4.Pt() - matchedGenJet->p4.Pt())/recoJets[iJet]->p4.Pt();
+                }
+                else if ((m_smearingMethod == JERSmearer::SmearingMethod::STOCHASTIC) || (!matchedGenJet)) {
+                    // match not found or stochastic method requested -> draw pT smearing factor from a normal distribution
+                    recoJets[iJet]->p4 *= 1 + std::normal_distribution<>(0, jetResolution)(m_randomNumberGenerator) * std::sqrt(std::max(jetResolutionScaleFactor * jetResolutionScaleFactor - 1, 0.0));
+                }
             }
         }
     }
