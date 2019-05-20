f43a5b03838eee7cd9cc6d4a87f7fb1899affa06
tfesenbecker <tabea.fessenbecker@student.kit.edu>
2019-05-20 15:29:36 +0200

    added xrootd paths to Daniels skims

--- cfg/excalibur/jec16/mc16_DYJets_madgraph/common.py
+++ cfg/excalibur/jec16/mc16_DYJets_madgraph/common.py
@@ -9,4 +9,5 @@ SE_PATH_PREFIXES = dict(
     srm_desy_dcache="srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user",
     local_gridka_nrg="/storage/gridka-nrg",
     local_desy_dcache="/pnfs/desy.de/cms/tier2/store/user",
+    xrootd_gridka_nrg="root://cmsxrootd-redirectors.gridka.de//store/user"
 )
--- cfg/excalibur/jec16/mc16_DYJets_madgraph/mc16_mm_BCDEFGH_DY2J_Madgraph.py
+++ cfg/excalibur/jec16/mc16_DYJets_madgraph/mc16_mm_BCDEFGH_DY2J_Madgraph.py
@@ -14,9 +14,11 @@ JEC='{}_{}'.format(JEC_BASE, JEC_VERSION)
 def config():
     cfg = configtools.getConfig('mc', 2016, CH, JEC=JEC, JER=JER)
     cfg["InputFiles"].set_input(
-        ekppath="{}/tberger/Skimming/MC-Summer16_metfix/Zll_DY2JetsToLL_M-50_madgraphMLM-pythia8_RunIISummer16/*.root".format(SE_PATH_PREFIXES['srm_desy_dcache']),
-        bmspath="{}/tberger/Skimming/MC-Summer16_metfix/Zll_DY2JetsToLL_M-50_madgraphMLM-pythia8_RunIISummer16/*.root".format(SE_PATH_PREFIXES['srm_desy_dcache']),
-        nafpath="{}/tberger/Skimming/MC-Summer16_metfix/Zll_DY2JetsToLL_M-50_madgraphMLM-pythia8_RunIISummer16/*.root".format(SE_PATH_PREFIXES['local_desy_dcache']),
+        # ekppath="{}/tberger/Skimming/MC-Summer16_metfix/Zll_DY2JetsToLL_M-50_madgraphMLM-pythia8_RunIISummer16/*.root".format(SE_PATH_PREFIXES['srm_desy_dcache']),
+        # bmspath="{}/tberger/Skimming/MC-Summer16_metfix/Zll_DY2JetsToLL_M-50_madgraphMLM-pythia8_RunIISummer16/*.root".format(SE_PATH_PREFIXES['srm_desy_dcache']),
+        # nafpath="{}/tberger/Skimming/MC-Summer16_metfix/Zll_DY2JetsToLL_M-50_madgraphMLM-pythia8_RunIISummer16/*.root".format(SE_PATH_PREFIXES['local_desy_dcache']),
+        path="{}/dsavoiu/Skimming/ZJet_DY2JetsToLL_Summer16-madgraphMLM_asymptotic_2016_TrancheIV_v6-v1_egmSSbackport/*.root".format(SE_PATH_PREFIXES['xrootd_gridka_nrg']),
+
     )
 
     cfg = configtools.expand(cfg, ['nocuts','basiccuts','finalcuts'], ['None', 'L1', 'L1L2L3'])
