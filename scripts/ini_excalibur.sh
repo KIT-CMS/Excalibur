3be425b815c58ddda84e8567fd49f268509b43ae
Christoph Heidecker <christoph.heidecker@cern.ch>
2020-02-25 17:07:39 +0100

    changed cheidecker settings

--- scripts/ini_excalibur.sh
+++ scripts/ini_excalibur.sh
@@ -67,7 +67,8 @@ if [ $USER = "cheidecker" ]; then
     export EXCALIBURBRILSSH="cheideck@lxplus.cern.ch"
     export EXCALIBUR_WORK=/portal/ekpbms3/home/cheidecker/excalibur_work
     export EXCALIBUR_SE="srm://cmssrm-kit.gridka.de:8443/srm/managerv2?SFN=/pnfs/gridka.de/cms/disk-only/store/user/cheidecker/Excalibur"
-    export HARRY_USERPC="lx26.etp.kit.edu"
+    export HARRY_USERPC="ekplx26.ekp.kit.edu"
+    export X509_USER_PROXY="/tmp/x509up_u12088"
 elif [ $USER = "tberger" ]; then
     export EXCALIBURBRILSSH="tberger@lxplus.cern.ch"
     export HARRY_REMOTE_USER="tberger"
