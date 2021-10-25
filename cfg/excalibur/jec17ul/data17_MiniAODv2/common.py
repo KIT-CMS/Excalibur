a5151dd1e7ffb670ff921d270fd91ae89c286423
RHofsaess <uaikd@student.kit.edu>
2021-10-25 15:25:03 +0200

    SE  access changed to xrd.

---
+++ cfg/excalibur/jec17ul/data17_MiniAODv2/common.py
@@ -0,0 +1,12 @@
+JEC_BASE='Summer19UL17'
+JEC_VERSION='V6'
+
+JER = None
+
+SE_PATH_PREFIXES = dict(
+    srm_gridka_nrg="srm://cmssrm-kit.gridka.de:8443/srm/managerv2?SFN=/pnfs/gridka.de/cms/disk-only/store/user",
+    xrootd_gridka_nrg="root://cmsxrootd-redirectors.gridka.de//store/user",
+    # xrootd_gridka_nrg="root://cmsxrootd-redirectors.gridka.de//pnfs/gridka.de/cms/disk-only/store/user",
+    local_gridka_nrg="/storage/gridka-nrg",
+)
+
